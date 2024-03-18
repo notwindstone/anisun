6d2c31fc304d006c8c020eec03ba17372ec69bc8
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-03-18 21:42:20 +0300

    Upgrade to third version API

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -1,11 +1,15 @@
 import { Text } from '@mantine/core';
 
 export async function Welcome() {
-    const res = await fetch('https://api.anilibria.tv/v2/getGenres');
+    const res = await fetch('https://api.anilibria.tv/v3/genres');
     const obj = await res.json();
     const genres = obj.map((genre: []) => (
             <Text>{genre}</Text>
         ));
+
+    const getRandomTitle = await fetch('https://api.anilibria.tv/v3/title/random');
+    const randomTitle = await getRandomTitle.json();
+    console.log(randomTitle.player);
 
     return (
         <>
