67c918b4751696a8f334a01123e25f83d46086ae
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-03-18 21:52:07 +0300

    bruh whatever

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -1,4 +1,6 @@
 import { Text } from '@mantine/core';
+import React, { useEffect, useState } from 'react';
+import ReactPlayer from 'react-player';
 
 export async function Welcome() {
     const res = await fetch('https://api.anilibria.tv/v3/genres');
@@ -11,9 +13,23 @@ export async function Welcome() {
     const randomTitle = await getRandomTitle.json();
     console.log(randomTitle.player.list[1].hls.fhd);
 
+    const [hasWindow, setHasWindow] = useState(false);
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            setHasWindow(true);
+        }
+    }, []);
+
     return (
         <>
             {genres}
+            {
+                hasWindow && <ReactPlayer
+                  src="https://www.youtube.com/watch?v=zohpogt56Bg"
+                  width="100%"
+                  height="100%"
+                  className={s.player} />
+            }
         </>
     );
 }
