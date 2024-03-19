cababc4df93d98ec7a78537ca130a16a10fa4a01
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-03-19 20:48:42 +0300

    IT WORKS! VideoPlayer embed with AniLibria anime

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Welcome } from '@/components/Welcome/Welcome';
 import { ColorSchemeToggle } from '@/components/ColorSchemeToggle/ColorSchemeToggle';
+import VideoEmbed from "@/components/VideoEmbed/VideoEmbed";
 
 export default function HomePage() {
   return (
     <>
       <Welcome />
+        <VideoEmbed />
       <ColorSchemeToggle />
     </>
   );
---
+++ components/VideoEmbed/VideoEmbed.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import VideoPlayer from '@/components/VideoPlayer/VideoPlayer';
+
+export default function VideoEmbed() {
+    return (
+        <>
+            <VideoPlayer src="https://cache.libria.fun/videos/media/ts/9284/3/720/4cc228b26307888f9cb0091ff233a6e3.m3u8" />
+        </>
+    );
+}
