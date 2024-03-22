04ee85f569b4d69d90454995893bdd2073ee3fb9
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-03-22 20:45:24 +0300

    Working autocomplete with debounced async calls. Thanks zvshka with help

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -12,33 +12,33 @@ interface TitleProps {
 }
 
 export function Search() {
-    const [value, setValue] = useDebouncedState('', 1000, { leading: true });
+    const [value, setValue] = useDebouncedState('', 300, { leading: true });
     const [data, setData] = useState([]);
 
-    const onChange = async (keyInput: string) => {
-        console.log(keyInput);
-        setValue(keyInput);
+    useEffect(() => {
+        const onChange = async (keyInput: string) => {
+            if (keyInput.length >= 3) {
+                const response = await fetch(`https://api.anilibria.tv/v3/title/search?search=${keyInput}&limit=5`);
+                const responseData = await response.json();
+                const titles = responseData.list.map((title: TitleProps) => (
+                    `${title.names.ru} / ${title.names.en}`
+                ));
+                setData(titles);
+            }
+        };
 
-        if (keyInput.length >= 3) {
-            const response = await fetch(`https://api.anilibria.tv/v3/title/search?search=${keyInput}&limit=5`);
-            const responseData = await response.json();
-            const titles = responseData.list.map((title: TitleProps) => (
-                `${title.names.ru} / ${title.names.en}`
-            ));
-            setData(titles);
-            console.log(data, value);
-        }
-    };
+        onChange(value)
+            .catch(console.error);
+    }, [value]);
 
     return (
         <>
             <Autocomplete
               data={data}
               defaultValue={value}
-              onChange={onChange}
+              onChange={(event) => setValue(event)}
               placeholder="Поиск"
             />
-            <Text>{value}</Text>
         </>
     );
 }
