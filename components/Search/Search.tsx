7652154f2875235aa3e46a6ceb3963ae02747248
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-03-22 20:32:54 +0300

    Initial code save. Trying to implement debounced API call

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import { useState } from 'react';
-import { Autocomplete } from '@mantine/core';
+import { useEffect, useState } from 'react';
+import { Autocomplete, Text } from '@mantine/core';
+import { useDebouncedState } from '@mantine/hooks';
 
 interface TitleProps {
     names: {
@@ -11,15 +12,15 @@ interface TitleProps {
 }
 
 export function Search() {
-    const [value, setValue] = useState('');
+    const [value, setValue] = useDebouncedState('', 1000, { leading: true });
     const [data, setData] = useState([]);
 
-    const onChange = async (e: string) => {
-        console.log(e);
-        setValue(e);
+    const onChange = async (keyInput: string) => {
+        console.log(keyInput);
+        setValue(keyInput);
 
-        if (e.length >= 3) {
-            const response = await fetch(`https://api.anilibria.tv/v3/title/search?search=${e}&limit=5`);
+        if (keyInput.length >= 3) {
+            const response = await fetch(`https://api.anilibria.tv/v3/title/search?search=${keyInput}&limit=5`);
             const responseData = await response.json();
             const titles = responseData.list.map((title: TitleProps) => (
                 `${title.names.ru} / ${title.names.en}`
@@ -28,25 +29,16 @@ export function Search() {
             console.log(data, value);
         }
     };
-    /*
-    const onChange = async (e) => {
-        const value = e.target;
-        console.log(e, e.key)
-        setQuery(value);
 
-        if (value.length > 3) {
-            const response = await fetch(`https://api.anilibria.tv/v3/title/search?search=${value}&limit=5`);
-            const data = await response.json();
-            console.log(data);
-        }
-    };
-     */
     return (
-        <Autocomplete
-          data={data}
-          value={value}
-          onChange={onChange}
-          placeholder="Поиск"
-        />
+        <>
+            <Autocomplete
+              data={data}
+              defaultValue={value}
+              onChange={onChange}
+              placeholder="Поиск"
+            />
+            <Text>{value}</Text>
+        </>
     );
 }
