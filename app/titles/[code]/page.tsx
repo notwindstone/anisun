52e3daa0fc98c84fc8e65d650868bf722ad42d91
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-03-24 09:38:43 +0300

    Get title by code

--- app/titles/[code]/page.tsx
+++ app/titles/[code]/page.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import VideoEmbed from '@/components/VideoEmbed/VideoEmbed';
 
 export default async function Page({ params }: { params: { code: string } }) {
-    const response = await fetch(`https://api.anilibria.tv/v3/title/search?search=${params.code}&limit=1`);
+    const response = await fetch(`https://api.anilibria.tv/v3/title?code=${params.code}`);
     const responseData = await response.json();
-    const animePlayer = responseData.list[0].player;
+    const animePlayer = responseData.player;
     const animeHost = `https://${animePlayer.host}`;
     const animeEpisodeVideo = animePlayer.list[1].hls.fhd;
 
