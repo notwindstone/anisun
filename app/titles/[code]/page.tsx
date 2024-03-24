47e14f41a3967903b3ce3d6efd8c10dc2bb8dd4c
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-03-24 09:46:09 +0300

    ResponseDataProps interface

--- app/titles/[code]/page.tsx
+++ app/titles/[code]/page.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import VideoEmbed from '@/components/VideoEmbed/VideoEmbed';
 
+interface ResponseDataProps {
+    player: {
+        host: string;
+        list: {
+            hls: {
+                fhd: string;
+                hd: string;
+                sd: string;
+            }
+        }[]
+    },
+}
+
 export default async function Page({ params }: { params: { code: string } }) {
     const response = await fetch(`https://api.anilibria.tv/v3/title?code=${params.code}`);
-    const responseData = await response.json();
+    const responseData: ResponseDataProps = await response.json();
     const animePlayer = responseData.player;
     const animeHost = `https://${animePlayer.host}`;
     const animeEpisodeVideo = animePlayer.list[1].hls.fhd;
