f5c6c82bbb4ceee87aea14be672c788d77ff525d
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-03-25 09:41:04 +0300

    Custom Vidstack controls

--- components/VideoPlayer/VideoPlayer.tsx
+++ components/VideoPlayer/VideoPlayer.tsx
@@ -1,6 +1,10 @@
 import '@vidstack/react/player/styles/default/theme.css';
-import { MediaPlayer, MediaProvider, Poster } from '@vidstack/react';
-import { PlayIcon } from '@vidstack/react/icons';
+import { ChapterTitle, Controls, Gesture, MediaPlayer, MediaProvider, Poster } from '@vidstack/react';
+import styles from './VideoPlayer.module.css';
+import * as Buttons from '@/components/VideoPlayer/VideoPlayerComponents/Buttons';
+import * as Menus from '@/components/VideoPlayer/VideoPlayerComponents/Menus';
+import * as Sliders from '@/components/VideoPlayer/VideoPlayerComponents/Sliders';
+import { TimeGroup } from '@/components/VideoPlayer/VideoPlayerComponents/TimeGroup';
 
 interface VideoPlayerProps {
     source: string;
@@ -9,20 +13,46 @@ interface VideoPlayerProps {
 
 export default function VideoPlayer({ source, preview }: VideoPlayerProps) {
     return (
-        <>
+        <div className={styles.wrapper}>
             <MediaPlayer
               title={source}
               src={source}
+              viewType="video"
             >
                 <MediaProvider>
-                    <Poster
-                      className="vds-poster"
-                      src={preview}
-                      alt="Anime episode poster"
-                    />
-                    <PlayIcon size={40} />
+                    <Poster src={preview} alt="Anime episode preview" />
                 </MediaProvider>
+                <Gestures />
+                <Controls.Root className={`${styles.controls} vds-controls`}>
+                    <div className="vds-controls-spacer" />
+                    <Controls.Group className={`${styles.controlsGroup} vds-controls-group`}>
+                        <Sliders.Time />
+                    </Controls.Group>
+                    <Controls.Group className={`${styles.controlsGroup} vds-controls-group`}>
+                        <Buttons.Play tooltipPlacement="top start" />
+                        <Buttons.Mute tooltipPlacement="top" />
+                        <Sliders.Volume />
+                        <TimeGroup />
+                        <ChapterTitle className="vds-chapter-title" />
+                        <div className="vds-controls-spacer" />
+                        <Menus.Settings placement="top end" tooltipPlacement="top" />
+                        <Buttons.PIP tooltipPlacement="top" />
+                        <Buttons.Fullscreen tooltipPlacement="top end" />
+                    </Controls.Group>
+                </Controls.Root>
             </MediaPlayer>
+        </div>
+    );
+}
+
+function Gestures() {
+    return (
+        <>
+            <Gesture className={styles.gesture} event="pointerup" action="toggle:paused" />
+            <Gesture className={styles.gesture} event="dblpointerup" action="toggle:fullscreen" />
+            <Gesture className={styles.gesture} event="pointerup" action="toggle:controls" />
+            <Gesture className={styles.gesture} event="dblpointerup" action="seek:-10" />
+            <Gesture className={styles.gesture} event="dblpointerup" action="seek:10" />
         </>
     );
 }
