d5e9d61432aae2c5a01e14bf53742c873fea927c
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-03-25 11:42:06 +0300

    Code save before removing all VideoPlayer files

--- components/VideoPlayer/VideoPlayerComponents/Menus.tsx
+++ components/VideoPlayer/VideoPlayerComponents/Menus.tsx
@@ -3,7 +3,6 @@ import type { ReactNode } from 'react';
 import {
     Menu,
     Tooltip,
-    useCaptionOptions,
     type MenuPlacement,
     type TooltipPlacement, useVideoQualityOptions,
 } from '@vidstack/react';
