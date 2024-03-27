8db931101eff7d290a15aa66445042e578e8292a
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-03-27 21:00:09 +0300

    Check if anime has player

--- app/titles/[code]/page.tsx
+++ app/titles/[code]/page.tsx
@@ -18,6 +18,17 @@ export default async function Page({ params }: { params: { code: string } }) {
     const response = await fetch(`https://api.anilibria.tv/v3/title?code=${params.code}`);
     const responseData: ResponseDataProps = await response.json();
     const animePlayer = responseData.player;
+
+    // Some anime titles don't have a player
+    if (Object.keys(animePlayer.list).length === 0) {
+        return (
+            <>
+                <div>{params.code}</div>
+                <div>К сожалению, онлайн-плеер для данного аниме недоступен.</div>
+            </>
+        );
+    }
+
     const animeHost = `https://${animePlayer.host}`;
     const animeHLS = animePlayer.list[1].hls;
 
