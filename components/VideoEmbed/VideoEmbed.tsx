5ce5b31b1be1e41d1a2e015cbfa4977462f2a86d
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-03-28 14:06:53 +0300

    I'm факинг genius

--- components/VideoEmbed/VideoEmbed.tsx
+++ components/VideoEmbed/VideoEmbed.tsx
@@ -5,23 +5,28 @@ import VideoPlayer from '@/components/VideoPlayer/VideoPlayer';
 interface VideoEmbedProps {
     host: string;
     source: {
-        fhd: string;
-        hd: string;
-        sd: string;
+        fhd?: string;
+        hd?: string;
+        sd?: string;
     };
     preview: string;
 }
 
+interface VideoPlaylistProps {
+    fhd?: string;
+    hd?: string;
+    sd?: string;
+}
+
 export default function VideoEmbed({ host, source, preview }: VideoEmbedProps) {
-    const playlist = {};
+    const playlist: VideoPlaylistProps = {};
 
     for (const [key, value] of Object.entries(source)) {
         if (value !== null) {
+            // @ts-ignore
             playlist[key] = host + value;
         }
     }
-
-    console.log(playlist);
 
     return (
         <>
--- components/VideoPlayer/VideoPlayer.tsx
+++ components/VideoPlayer/VideoPlayer.tsx
@@ -6,20 +6,34 @@ import styles from './VideoPlayer.module.css';
 
 interface VideoPlayerProps {
     source: {
-        fhd: string;
-        hd: string;
-        sd: string;
+        fhd?: string;
+        hd?: string;
+        sd?: string;
     };
     preview: string;
 }
 
 export default function VideoPlayer({ source, preview }: VideoPlayerProps) {
+    const playlistSource = '#EXTM3U\n' +
+        '#EXT-X-VERSION:3\n' +
+        '\n' +
+        '#EXT-X-STREAM-INF:RESOLUTION=720x480\n' +
+        'https://cache.libria.fun/videos/media/ts/4217/1/480/c6889f9b0c1a9a4c7971925efe2dbfe5.m3u8\n' +
+        '#EXT-X-STREAM-INF:RESOLUTION=1280x720\n' +
+        'https://cache.libria.fun/videos/media/ts/4217/1/720/ca0fb100a3a8b41842cc4a062ad50520.m3u8';
+
+    const blob = new Blob([playlistSource], {
+        type: 'application/x-mpegurl',
+    });
+
+    const url = URL.createObjectURL(blob);
+
     return (
         <div className={styles.wrapper}>
             <MediaPlayer
               className={styles.player}
               title="1234"
-              src="https://raw.githubusercontent.com/windstone-aristotle-yellow/Immensity/main/main.m3u8"
+              src={{ src: url, type: 'application/x-mpegurl' }}
               viewType="video"
             >
                 <MediaProvider>
