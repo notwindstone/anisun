1ccde8f279b84ad18e309d8bc1d8eeb4b95b1bc0
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-03-29 15:13:34 +0300

    Refactor start

--- app/titles/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-    return (
-        <section>
-            {children}
-        </section>
-    );
-}
