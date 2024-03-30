df3d99dff67640543c6b5b7ef5e8c3e235c85c83
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-03-30 21:55:09 +0300

    Comment get method works

--- src/api/comments/getComments.ts
+++ src/api/comments/getComments.ts
@@ -3,7 +3,7 @@ import { comments } from "@/db/schema";
 import { asc } from 'drizzle-orm';
 
 export const getComments = async () => {
-    const data = await db.select().from(comments).orderBy(asc(comments.id));
+    const data = await db.select().from(comments).orderBy(asc(comments));
 
     return data;
 };
