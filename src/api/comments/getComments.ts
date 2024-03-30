15baf8bcad6d17e9d7e81abd7ad4d50ccd28fe5c
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-03-30 21:36:18 +0300

    getComments method

---
+++ src/api/comments/getComments.ts
@@ -0,0 +1,9 @@
+import db from "@/db/drizzle";
+import { comments } from "@/db/schema";
+import { asc } from 'drizzle-orm';
+
+export const getComments = async () => {
+    const data = await db.select().from(comments).orderBy(asc(comments.id));
+
+    return data;
+};
