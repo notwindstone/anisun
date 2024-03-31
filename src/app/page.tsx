5974b91dbf54ecb608c1b021061b0e434468c216
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-03-31 10:14:42 +0300

    похуй на гайд, сделаю сам

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import {getComments} from "@/api/comments/getComments";
-import Comments from "@/components/Comments/Comments";
 
 export default async function Home() {
     const data = await getComments();
-    return <Comments comments={data} />
+    return <>Test</>
 }
