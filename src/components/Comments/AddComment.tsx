fbe45507a4a985ff31016de02e74f95b054adb8e
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-04-02 19:05:30 +0300

    Comments addition

--- src/components/Comments/AddComment.tsx
+++ src/components/Comments/AddComment.tsx
@@ -1,12 +1,27 @@
 import {useRef} from "react";
 import {Button, Group, Textarea} from "@mantine/core";
 import { IconMessage } from "@tabler/icons-react";
+import {comments} from "@/api/comments/comments";
+import {nanoid} from "nanoid";
 
 export default function AddComment() {
     const ref = useRef<HTMLTextAreaElement>(null);
 
     const handleSubmit = async () => {
-        console.log(ref.current?.value)
+        const input = ref.current?.value ?? ""
+        const uuid = nanoid()
+        const date = new Date().toJSON()
+
+        await comments.add(
+            uuid,
+            "ookami-to-koushinryou-merchant-meets-the-wise-wolf",
+            "https://tabler.io/packages/logo-figma.svg",
+            "windstone",
+            date,
+            0,
+            0,
+            input
+        )
     }
 
     return (
