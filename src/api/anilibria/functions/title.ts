ccb0cfe6e9882c3a735016b63bd8c4e9a75a56c1
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-04-02 13:13:15 +0300

    title function fix

--- src/api/anilibria/functions/title.ts
+++ src/api/anilibria/functions/title.ts
@@ -1,31 +1,31 @@
 import axios from "axios";
-import {anilibria} from "@/api/anilibria/anilibria";
+import {host} from "@/api/anilibria/functions/host";
 
-const host = anilibria.host.api()
+const anilibriaHost = host.api()
 
 export const title = {
     id: async function (id: number) {
-        return (await axios.get(`${host}title?id=${id}`)).data
+        return (await axios.get(`${anilibriaHost}title?id=${id}`)).data
     },
     code: async function (code: string) {
-        return (await axios.get(`${host}title?code=${code}`)).data
+        return (await axios.get(`${anilibriaHost}title?code=${code}`)).data
     },
     torrent_id: async function (torrent_id: string[]) {
-        return (await axios.get(`${host}title?torrent_id=${torrent_id}`)).data
+        return (await axios.get(`${anilibriaHost}title?torrent_id=${torrent_id}`)).data
     },
     filter: async function (filter: string[]) {
-        return (await axios.get(`${host}title?filter=${filter}`)).data
+        return (await axios.get(`${anilibriaHost}title?filter=${filter}`)).data
     },
     remove: async function (remove: string[]) {
-        return (await axios.get(`${host}title?remove=${remove}`)).data
+        return (await axios.get(`${anilibriaHost}title?remove=${remove}`)).data
     },
     include: async function (include: string[]) {
-        return (await axios.get(`${host}title?include=${include}`)).data
+        return (await axios.get(`${anilibriaHost}title?include=${include}`)).data
     },
     description_type: async function (description_type: string) {
-        return (await axios.get(`${host}title?description_type=${description_type}`)).data
+        return (await axios.get(`${anilibriaHost}title?description_type=${description_type}`)).data
     },
     playlist_type: async function (playlist_type: string) {
-        return (await axios.get(`${host}title?playlist_type=${playlist_type}`)).data
+        return (await axios.get(`${anilibriaHost}title?playlist_type=${playlist_type}`)).data
     },
 }
