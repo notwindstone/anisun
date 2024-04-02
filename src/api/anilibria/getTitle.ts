dc0b72ea25a7b3075716e94fa58536f097a75639
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-04-02 12:43:56 +0300

    getTitle return fetched data

--- src/api/anilibria/getTitle.ts
+++ src/api/anilibria/getTitle.ts
@@ -1,30 +1,31 @@
 import {getHost} from "@/api/anilibria/getHost";
+import axios from "axios";
 
 const host = getHost.api()
 
 export const getTitle = {
     id: async function (id: number) {
-        return `${host}title?id=${id}`
+        return await axios.get(`${host}title?id=${id}`)
     },
     code: async function (code: string) {
-        return `${host}title?code=${code}`
+        return await axios.get(`${host}title?code=${code}`)
     },
     torrent_id: async function (torrent_id: string) {
-        return `${host}title?torrent_id=${torrent_id}`
+        return await axios.get(`${host}title?torrent_id=${torrent_id}`)
     },
     filter: async function (filter: string) {
-        return `${host}title?filter=${filter}`
+        return await axios.get(`${host}title?filter=${filter}`)
     },
     remove: async function (remove: string) {
-        return `${host}title?remove=${remove}`
+        return await axios.get(`${host}title?remove=${remove}`)
     },
     include: async function (include: string) {
-        return `${host}title?include=${include}`
+        return await axios.get(`${host}title?include=${include}`)
     },
     description_type: async function (description_type: string) {
-        return `${host}title?description_type=${description_type}`
+        return await axios.get(`${host}title?description_type=${description_type}`)
     },
     playlist_type: async function (playlist_type: string) {
-        return `${host}title?playlist_type=${playlist_type}`
+        return await axios.get(`${host}title?playlist_type=${playlist_type}`)
     },
 }
