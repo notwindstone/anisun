6e79f76ae4f6d22acdb9fd784677e62802c4a362
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-04-02 16:57:50 +0300

    Comment section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,13 @@
 "use client"
 
 import Link from "next/link";
-import {comments} from "@/api/comments/comments";
-import {Avatar, Text} from "@mantine/core";
-import {useEffect, useState} from "react";
-import {nanoid} from "nanoid";
-
-async function getComments() {
-    const uuid = nanoid()
-    // const date = new Date().toJSON()
-
-    return await comments.get("ookami-to-koushinryou-merchant-meets-the-wise-wolf")
-}
+import Comments from "@/components/Comments/Comments";
 
 export default function Home() {
-    const [commentsSection, setCommentsSection] = useState()
-    const [refreshToken, setRefreshToken] = useState(Math.random())
-
-    useEffect(() => {
-        const refreshComments = async () => {
-            const data = await getComments()
-            const commentsData = data.map((comment) => {
-                return (
-                    <div key={comment.uuid}>
-                        <Text>{comment.uuid}</Text>
-                        <Text>{comment.title}</Text>
-                        <Avatar src={comment.avatar} size={64} />
-                        <Text>{comment.username}</Text>
-                        <Text>{comment.date}</Text>
-                        <Text>{comment.likes}</Text>
-                        <Text>{comment.dislikes}</Text>
-                        <Text>{comment.message}</Text>
-                    </div>
-                )
-            })
-            // @ts-ignore
-            setCommentsSection(commentsData)
-            setTimeout(() => setRefreshToken(Math.random()), 3000)
-        }
-        refreshComments().then()
-    }, [refreshToken]);
-
     return (
         <>
             <Link href="/titles">Test</Link>
-            {commentsSection}
+            <Comments />
         </>
     )
 }
