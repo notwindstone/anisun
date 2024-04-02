2492e1d458ee731a809b198224aca58dfb1025dd
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-04-02 18:51:50 +0300

    Comment add button

--- src/components/Comments/AddComment.tsx
+++ src/components/Comments/AddComment.tsx
@@ -1,16 +1,26 @@
 import {useRef} from "react";
-import {Textarea} from "@mantine/core";
+import {Button, Group, Textarea} from "@mantine/core";
+import { IconMessage } from "@tabler/icons-react";
 
 export default function AddComment() {
     const ref = useRef<HTMLTextAreaElement>(null);
 
+    const handleSubmit = async () => {
+        console.log(ref.current?.value)
+    }
+
     return (
-        <Textarea
-          ref={ref}
-          placeholder="Autosize with no rows limit"
-          label="Autosize with no rows limit"
-          autosize
-          minRows={2}
-        />
+        <Group>
+            <Textarea
+                ref={ref}
+                placeholder="Autosize with no rows limit"
+                autosize
+                required
+                minRows={2}
+            />
+            <Button onClick={handleSubmit} variant="light">
+                <IconMessage />
+            </Button>
+        </Group>
     )
 }
