2ece80c52d98b6aa71051fb6e8a5de54f14d0a28
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-04-02 19:00:56 +0300

    Sort comments by newest

--- src/api/comments/functions/get.ts
+++ src/api/comments/functions/get.ts
@@ -2,9 +2,14 @@
 
 import db from "@/db/drizzle";
 import { comments } from "@/db/schema";
-import {asc, eq} from 'drizzle-orm';
+import { desc, eq } from 'drizzle-orm';
 
 export const get = async (title: string) => {
-    const data = await db.select().from(comments).where(eq(comments.title, title));
+    const data =
+        await db
+            .select()
+            .from(comments)
+            .where(eq(comments.title, title))
+            .orderBy(desc(comments.date));
     return data;
 };
