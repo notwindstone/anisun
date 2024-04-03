e9fd72fc6822b115e8c0c8ee923e8b75df6bb6c9
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-04-03 14:15:49 +0300

    Infinite query initial code

--- src/components/Comments/Comments.tsx
+++ src/components/Comments/Comments.tsx
@@ -3,22 +3,46 @@
 import {comments} from "@/api/comments/comments";
 import {Button, Loader, Skeleton} from "@mantine/core";
 import AddComment from "@/components/Comments/AddComment";
-import {useQuery} from "@tanstack/react-query";
+import {useInfiniteQuery, useQuery} from "@tanstack/react-query";
 import Comment from "@/components/Comments/Comment";
 import {useState} from "react";
 
 export default function Comments() {
     const [limit, setLimit] = useState(8)
 
-    const { isPending, data } = useQuery({
-        queryKey: ["comments", limit],
-        queryFn: async () => {
-            return await comments.get("ookami-to-koushinryou-merchant-meets-the-wise-wolf", limit)
+    const getComments = async ({ pageParam } : { pageParam: number }) => {
+        return await comments.get("ookami-to-koushinryou-merchant-meets-the-wise-wolf", pageParam)
+    }
+
+    const {
+        isPending,
+        data,
+        error,
+        fetchNextPage,
+        hasNextPage,
+        isFetching,
+        isFetchingNextPage,
+        status,
+    } = useInfiniteQuery({
+        queryKey: ["comments"],
+        queryFn: getComments,
+        initialPageParam: 0,
+        getNextPageParam: (lastPage, allPages, lastPageParam) => {
+            if (lastPage.length === 0) {
+                return undefined
+            }
+            return lastPageParam + 1
+        },
+        getPreviousPageParam: (firstPage, allPages, firstPageParam) => {
+            if (firstPageParam <= 1) {
+                return undefined
+            }
+            return firstPageParam - 1
         },
         refetchInterval: 5000
     })
 
-    const commentsData = data ?? []
+    const commentsData = data?.pages ?? []
 
     let commentsSection = commentsData.map((comment) => {
         return (
@@ -32,6 +56,7 @@ export default function Comments() {
             {isPending && <Loader color="blue" />}
             {commentsSection}
             <Button onClick={() => setLimit(limit + 8)}>Load</Button>
+
         </>
     )
 }
