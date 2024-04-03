1100560ec60badc8ff20adb0f787a3b29abf4980
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-04-03 19:51:34 +0300

    Sign In & Sign Up pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import Link from "next/link";
 import Comments from "@/components/Comments/Comments";
+import {SignedIn, SignedOut, SignInButton, UserButton} from "@clerk/nextjs";
 
 export default async function Home() {
     return (
         <>
             <Link href="/titles">Test</Link>
+            <SignedIn>
+                <UserButton />
+            </SignedIn>
+            <SignedOut>
+                <SignInButton />
+            </SignedOut>
         </>
     )
 }
