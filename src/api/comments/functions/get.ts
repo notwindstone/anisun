344de0cbcb374f9c66ae09c7c0f33406d240bc30
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-04-04 17:16:24 +0300

    Finally... Load 8 initial parent comments and all their child comments

--- src/api/comments/functions/get.ts
+++ src/api/comments/functions/get.ts
@@ -2,41 +2,37 @@
 
 import db from "@/db/drizzle";
 import { comments } from "@/db/schema";
-import {and, desc, eq, exists, isNull} from 'drizzle-orm';
+import {and, desc, eq, isNull} from 'drizzle-orm';
 
 export const get = async ({ title, nextCursor = 0 }: { title: string, nextCursor: number }) => {
     const initialLimit = nextCursor === 0 ? 8 : nextCursor
+
     const data =
         await db
             .select()
             .from(comments)
             .where(
                 and(
+                    eq(comments.title, title),
                     isNull(comments.parentuuid)
                 )
             )
             .orderBy(desc(comments.createdAt))
-            //.limit(initialLimit)
-            //.offset(nextCursor);
-
-    const test = data.map((test1) => {
-
-            return test1.parentuuid
-
-    })
-
-    const childComments =
-        await db
-            .select()
-            .from(comments)
-            .where(eq(comments.parentuuid, comments.parentuuid))
-
-    let mergedData = new Set([...data, ...childComments])
-
-    console.log(data.length, mergedData.size, test)
+            .limit(initialLimit)
+            .offset(nextCursor);
 
     if (data.length === 0) {
         return { data: null, nextCursor: nextCursor + 8 }
     }
+
+    for (const comment of data) {
+        const childComments = await db
+            .select()
+            .from(comments)
+            .where(eq(comments.parentuuid, comment.uuid))
+
+        data.push(...childComments)
+    }
+
     return { data: data, nextCursor: nextCursor + 8 };
 };
