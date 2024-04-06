b08ce737608081371f6a7cd6d6eaf28810f13238
windstone-aristotle-yellow <91563727+windstone-aristotle-yellow@users.noreply.github.com>
2024-04-06 23:03:54 +0300

    Bruh Vercel Fix #3

--- src/app/titles/[code]/page.tsx
+++ src/app/titles/[code]/page.tsx
@@ -23,7 +23,8 @@ interface ResponseProps {
 }
 
 export default async function Page({ params }: { params: { code: string } }) {
-    const response: ResponseProps = (await axios.get(`https://api.anilibria.tv/v3/title?code=${params.code}`)).data
+    const responseNonData: ResponseProps = await axios.get(`https://api.anilibria.tv/v3/title?code=${params.code}`)
+    const response = await responseNonData.data
     const animePlayer = response.player;
 
     // Некоторые аниме тайтлы не имеют плеера
