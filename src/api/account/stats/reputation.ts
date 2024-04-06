dbea45702205209c4438d1b3945d41e5ace311a0
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-04-06 20:21:42 +0300

    account reputation

--- src/api/account/stats/reputation.ts
+++ src/api/account/stats/reputation.ts
@@ -5,18 +5,20 @@ import { comments } from "@/db/schema";
 import { eq } from 'drizzle-orm';
 
 export const reputation = async ({ userid }: { userid: string }) => {
-    const likes =
+    const commentsData =
         await db
             .select()
             .from(comments)
             .where(eq(comments.userid, userid))
 
-    const dislikes =
-        await db
-            .select()
-            .from(comments)
-            .where(eq(comments.userid, userid))
+    let likes = 0
+    let dislikes = 0
 
-    const reputation = likes.length + dislikes.length
+    for (const comment of commentsData) {
+        likes = likes + (comment.likes?.length ?? 0)
+        dislikes = dislikes + (comment.dislikes?.length ?? 0)
+    }
+
+    const reputation = likes - dislikes
     return { data: reputation };
 };
