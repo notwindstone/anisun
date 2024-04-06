021ea4806368b15c13010c4490178e3baaa3ea5d
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-04-06 20:42:57 +0300

    Vercel

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,16 +1,16 @@
-/*
+
 import { Pool } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
 
 const pool = new Pool({ connectionString: process.env.NEON_DATABASE_URL });
 
 const db = drizzle(pool);
- */
 
+/*
 import postgres from "postgres";
 import {drizzle} from "drizzle-orm/postgres-js";
 
 const queryClient = postgres(process.env.POSTGRESQL_DATABASE_URL!);
 const db = drizzle(queryClient);
-
+ */
 export default db
