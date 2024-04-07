7c2a046ae65f287736ba67f30fe71bd519ece7a3
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-04-07 11:00:11 +0300

    holy shit i never thought that Client pages can be async

--- src/app/titles/[code]/page.tsx
+++ src/app/titles/[code]/page.tsx
@@ -3,6 +3,7 @@ import VideoEmbed from "@/components/VideoEmbed/VideoEmbed";
 import Link from "next/link";
 import {anilibria} from "@/api/anilibria/anilibria";
 import Comments from "@/components/Comments/Comments";
+import {useQuery} from "@tanstack/react-query";
 
 interface ResponseProps {
     names: {
@@ -22,9 +23,24 @@ interface ResponseProps {
 }
 
 export default async function Page({ params }: { params: { code: string } }) {
-    const response: ResponseProps = await anilibria.title.code(params.code)
-    const animePlayer = response.player;
-    console.log(params.code)
+    const { isFetching, data } = useQuery({
+        queryKey: ['anime', params.code],
+        queryFn: async () => fetchAnime(params.code),
+    });
+
+    async function fetchAnime(code: string) {
+        const response: ResponseProps = await anilibria.title.code(code)
+        return response
+    }
+
+    if (!data) {
+        return (
+            <>Ничего не найдено</>
+        )
+    }
+
+    const animePlayer = data.player;
+
     // Некоторые аниме тайтлы не имеют плеера
     if (Object.keys(animePlayer.list).length === 0) {
         return (
@@ -40,7 +56,7 @@ export default async function Page({ params }: { params: { code: string } }) {
             <Link href="/titles">Вернуться</Link>
             <div>{params.code}</div>
             <VideoEmbed
-              title={response.names.ru}
+              title={data.names.ru}
               player={animePlayer}
               preview="https://anilibria.tv/storage/releases/episodes/previews/9542/1/DMzcnlKyg89dRv5f__86bf22cbc0faac3d42cc7b87ea8c712f.jpg"
             />
