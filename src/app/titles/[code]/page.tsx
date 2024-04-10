5ad7615b5b80f91f396cd44b20bc267343988ab4
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-04-10 11:44:48 +0300

    Initial dynamic metadata for anime title pages

--- src/app/titles/[code]/page.tsx
+++ src/app/titles/[code]/page.tsx
@@ -1,9 +1,23 @@
+import {Metadata} from "next";
+
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
 
 import React from 'react';
 import VideoEmbed from "@/components/VideoEmbed/VideoEmbed";
 import Link from "next/link";
 import Comments from "@/components/Comments/Comments";
+import {anilibria} from "@/lib/anilibria/anilibria";
+
+export async function generateMetadata({ params }: { params: { code: string } }): Promise<Metadata> {
+    const code = params.code
+
+    const anime = await anilibria.title.code(code)
+
+    return {
+        title: anime.names.ru,
+        description: anime.description,
+    }
+}
 
 export default async function Page({ params }: { params: { code: string } }) {
     return (
