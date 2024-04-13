1c4eb0e82c9e9ceb10f3c34628b0e5081db2f273
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-04-13 21:47:33 +0300

    Working comment addition

--- src/components/Comments/AddComment/AddComment.tsx
+++ src/components/Comments/AddComment/AddComment.tsx
@@ -3,7 +3,9 @@ import {useRef, useState} from "react";
 import {useQueryClient} from "@tanstack/react-query";
 import {ActionIcon, Group, Textarea} from "@mantine/core";
 import {IconMessage} from "@tabler/icons-react";
-import {handleAdd} from "@/utils/comments/handleAdd";
+import {notify} from "@/utils/notify/notify";
+import {nanoid} from "nanoid";
+import {comments} from "@/lib/comments/comments";
 
 export function AddComment({ title, parentUUID }: { title: string, parentUUID: string | null }) {
     const { isLoaded, isSignedIn, user } = useUser();
@@ -13,6 +15,61 @@ export function AddComment({ title, parentUUID }: { title: string, parentUUID: s
     const queryClient = useQueryClient()
 
     const isUser = isLoaded && isSignedIn
+
+    const handleSubmit = async () => {
+        if (delayed) {
+            return notify.delay()
+        }
+
+        if (!isUser) {
+            return notify.notAuthenticated()
+        }
+
+        const message = ref.current?.value ?? ""
+
+        if (message.length < 2 || message.length > 2000) {
+            return notify.incorrectInput()
+        }
+
+        const uuid = nanoid()
+        const createdAt = new Date().toJSON()
+
+        const userId = user?.id
+        const username = user?.username ?? "Пользователь без никнейма"
+        const avatar = user?.imageUrl
+
+        await comments.add(
+            uuid,
+            /*
+             * Чтобы комментарии не уходили ниже 3-го уровня,
+             * я в качестве parentuuid использую айди прародителя комментария
+             *
+             * Схема: Комментарий 1
+             *            |-> Ответ на комментарий
+             *                |-> Ответ на ответ
+             *                |-> Ответ на ответ на ответ
+             */
+            parentUUID,
+            title,
+            userId,
+            username,
+            avatar,
+            createdAt,
+            [],
+            [],
+            message,
+            false,
+            false,
+        )
+
+        notify.done()
+
+        setDelayed(true)
+
+        setTimeout(() => {
+            setDelayed(false)
+        }, 5000)
+    }
 
     return (
         <Group>
@@ -33,7 +90,7 @@ export function AddComment({ title, parentUUID }: { title: string, parentUUID: s
             {
                 isUser
                     ? (
-                        <ActionIcon type="button" onClick={() => handleAdd({ title, parentUUID })} variant="light">
+                        <ActionIcon type="button" onClick={handleSubmit} variant="light">
                             <IconMessage />
                         </ActionIcon>
                     ) : (
--- src/utils/comments/handleAdd.ts
+++
@@ -1,3 +0,0 @@
-export const handleAdd = async ({ title, parentUUID }: { title: string, parentUUID: string | null }) => {
-    console.log(title, parentUUID)
-}
