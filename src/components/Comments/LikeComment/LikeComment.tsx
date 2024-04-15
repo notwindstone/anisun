1760020df963ad24ed495555ecbf8b40b6cf35c0
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-04-15 18:27:37 +0300

    Save code before moving user related code from AddComment to CommentList component

--- src/components/Comments/LikeComment/LikeComment.tsx
+++ src/components/Comments/LikeComment/LikeComment.tsx
@@ -2,7 +2,9 @@ import {IconCaretUpFilled} from "@tabler/icons-react";
 import {ActionIcon} from "@mantine/core";
 
 export function LikeComment({ uuid, likes, sendComment }: { uuid: string, likes: unknown[] | null, sendComment: (likes: string[]) => void }) {
+    const handleLike = async () => {
 
+    }
 
     return (
         <ActionIcon>
