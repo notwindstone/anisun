b8b319ecf831fe7d946c1d3a23b450aa8c39d5ff
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-04-18 12:02:53 +0300

    brooooooo

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    KODIK_TOKEN: process.env.KODIK_TOKEN,
-  },
   experimental: {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
   },
