168a0098adb58ba6ecb0a3d9ed20ff1e85fca535
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-04-18 11:47:53 +0300

    vercel

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     return [
       {
         // matching all API routes
-        source: "/api/:path",
+        source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
