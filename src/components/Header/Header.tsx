17512d173d6b70ed193b4420c107576629f1c4c8
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-04-29 15:20:38 +0300

    Mobile modal initial code

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,20 @@
 "use client"
 
-import {ActionIcon, Button, Flex, Group, Image, Title, UnstyledButton} from "@mantine/core";
+import {ActionIcon, Button, em, Flex, Group, Image, Title, UnstyledButton} from "@mantine/core";
 import NextImage from "next/image";
 import globalVariables from "@/configs/globalVariables.json";
 import Link from "next/link";
 import SearchBar from "@/components/SearchBar/SearchBar";
 import {IconSearch, IconUser} from "@tabler/icons-react";
+import {useMediaQuery} from "@mantine/hooks";
 
 export default function Header() {
+    const isMobile = useMediaQuery(`(max-width: ${em(750)})`);
+
+    if (isMobile) {
+        return
+    }
+
     return (
         <header>
             <Flex
@@ -18,6 +25,7 @@ export default function Header() {
                     <Image
                         alt="Animeth website icon"
                         src="/favicon.png"
+                        radius="xl"
                         w={48}
                         h={48}
                         component={NextImage}
@@ -27,7 +35,7 @@ export default function Header() {
                         blurDataURL={globalVariables.imagePlaceholder}
                     />
                     <Title>Animeth</Title>
-                    <UnstyledButton>Страницы</UnstyledButton>
+                    <UnstyledButton>Перейти</UnstyledButton>
                 </Group>
                 <Group>
                     <ActionIcon radius="xl">
--- src/components/MobileModal/MobileModal.tsx
+++ src/components/MobileModal/MobileModal.tsx
@@ -1,18 +1,34 @@
 'use client'
 
 import {useMediaQuery} from "@mantine/hooks";
-import {em} from "@mantine/core";
+import {ActionIcon, Button, em, Flex, Text} from "@mantine/core";
+import classes from './MobileModal.module.css';
+import {IconMenu2, IconSearch, IconUser} from "@tabler/icons-react";
 
 export default function MobileModal() {
     const isMobile = useMediaQuery(`(max-width: ${em(750)})`);
 
+    if (!isMobile) {
+        return
+    }
+
     return (
-        <>
-            {
-                isMobile && (
-                    <div>mobile modal</div>
-                )
-            }
-        </>
+        <div className={classes.wrapper}>
+            <Flex
+                className={classes.root}
+                justify="space-between"
+                align="center"
+            >
+                <ActionIcon>
+                    <IconMenu2 />
+                </ActionIcon>
+                <Button leftSection={<IconSearch />}>
+                    <Text>Поиск</Text>
+                </Button>
+                <ActionIcon>
+                    <IconUser />
+                </ActionIcon>
+            </Flex>
+        </div>
     );
 }
