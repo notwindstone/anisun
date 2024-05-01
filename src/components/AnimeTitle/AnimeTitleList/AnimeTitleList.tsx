7fef7809539b1b32ae34871e802a60fa43ae07b9
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-05-01 19:02:16 +0300

    Somewhat working tanstack virtual list

--- src/components/AnimeTitle/AnimeTitleList/AnimeTitleList.tsx
+++ src/components/AnimeTitle/AnimeTitleList/AnimeTitleList.tsx
@@ -2,11 +2,11 @@
 
 import {useInfiniteQuery} from "@tanstack/react-query";
 import {client} from "@/lib/shikimori/client";
-import {InView} from "react-intersection-observer";
-import {Divider, Grid, rem} from "@mantine/core";
+import {Grid, rem, Title} from "@mantine/core";
 import AnimeTitleCard from "@/components/AnimeTitle/AnimeTitleCard/AnimeTitleCard";
 import AnimeVideoSkeleton from "@/components/Skeletons/AnimeVideoSkeleton/AnimeVideoSkeleton";
-import classes from './AnimeTitleList.module.css';
+import {useVirtualizer, useWindowVirtualizer} from '@tanstack/react-virtual';
+import React, {useEffect} from "react";
 
 export default function AnimeTitleList() {
     const shikimori = client()
@@ -15,7 +15,6 @@ export default function AnimeTitleList() {
         status,
         fetchNextPage,
         isFetchingNextPage,
-        error,
     } = useInfiniteQuery({
         queryKey: ['animeTitlesList'],
         queryFn: ({ pageParam }) => getAnimeList(pageParam),
@@ -23,15 +22,6 @@ export default function AnimeTitleList() {
         getNextPageParam: (lastPage) =>
             lastPage.nextCursor,
     })
-
-    async function getAnimeList(pageParam: number) {
-        const animeList = (await shikimori.animes.list({
-            limit: 16,
-            page: pageParam
-        })).animes
-
-        return { animeList: animeList, nextCursor: pageParam + 1 }
-    }
 
     const skeletons = Array.from({ length: 16 }).map((_skeleton, index) => {
         return (
@@ -41,50 +31,54 @@ export default function AnimeTitleList() {
         )
     })
 
-    const animeTitleListSection = status === 'pending' ? (
-        <>{skeletons}</>
-    ) : status === 'error' ? (
-        <>Error: {error.message}</>
-    ) : (
-        <>
-            {
-                data?.pages.map((page) => {
-                    return page.animeList.map((anime) => {
-                        return (
-                            <Grid.Col span={{ base: 12, xs: 6, sm: 4, lg: 3 }} key={anime.id}>
-                                <AnimeTitleCard anime={anime} />
-                            </Grid.Col>
-                        )
-                    })
-                })
-            }
+    const parentRef = React.useRef()
 
-        </>
-    )
+    const allRows = data ? data?.pages.map((d) => d.animeList) : []
+
+    const rowVirtualizer = useVirtualizer({
+        count: allRows.length,
+        // @ts-ignore
+        getScrollElement: () => parentRef.current,
+        estimateSize: () => 500,
+    })
+    async function getAnimeList(pageParam: number) {
+        const animeList = (await shikimori.animes.list({
+            limit: 16,
+            page: pageParam
+        })).animes
+
+        return { animeList: animeList, nextCursor: pageParam + 1 }
+    }
 
     return (
         <>
-            <Grid p={rem(32)}>
-                {animeTitleListSection}
-                {
-                    isFetchingNextPage && skeletons
-                }
-            </Grid>
-            <InView
-                onChange={(inView) => {
-                    if (!inView) {
-                        return
-                    }
-
-                    if (isFetchingNextPage) {
-                        return
-                    }
-
-                    fetchNextPage().then()
-                }}
-            >
-                <Divider className={classes.divider} h={8} />
-            </InView>
+            {/* @ts-ignore */}
+            <div ref={parentRef} className="List">
+                <div
+                    style={{
+                        height: `${rowVirtualizer.getTotalSize()}px`,
+                        width: '100%',
+                        position: 'relative',
+                    }}
+                >
+                    {rowVirtualizer.getVirtualItems().map((item) => (
+                        <Grid
+                            key={item.key}
+                            style={{
+                                position: 'absolute',
+                                top: 0,
+                                left: 0,
+                                height: `fit-content`,
+                                transform: `translateY(${
+                                    item.start - rowVirtualizer.options.scrollMargin
+                                }px)`,
+                            }}
+                        >
+                            <Grid.Col>{item.index}{console.log(allRows[item.index])}</Grid.Col>
+                        </Grid>
+                    ))}
+                </div>
+            </div>
         </>
     )
 }
