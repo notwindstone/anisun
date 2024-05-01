eb0a2246f25fc490e929afa3304bb7709b01a998
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-05-01 16:40:04 +0300

    Anime title card fixes

--- src/components/AnimeTitle/AnimeTitleCard/AnimeTitleCard.tsx
+++ src/components/AnimeTitle/AnimeTitleCard/AnimeTitleCard.tsx
@@ -1,7 +1,8 @@
 import {AnimeType} from "@/types/Shikimori/Responses/Types/AnimeType"
-import {Avatar, Flex, Group, Image, Stack, Text, Title} from "@mantine/core";
+import {AspectRatio, Avatar, Flex, Group, Image, Stack, Text, Title} from "@mantine/core";
 import NextImage from "next/image";
 import globalVariables from '@/configs/globalVariables.json';
+import classes from './AnimeTitleCard.module.css';
 
 export default function AnimeTitleCard({ anime }: { anime: AnimeType }) {
     const poster = anime.poster?.originalUrl ?? '/missing-image.png'
@@ -9,26 +10,29 @@ export default function AnimeTitleCard({ anime }: { anime: AnimeType }) {
     return (
         <div>
             <Stack>
-                <Image
-                    alt={`Постер к ${anime.name}`}
-                    src={poster}
-                    component={NextImage}
-                    w={280}
-                    h={157}
-                    width={280}
-                    height={157}
-                    placeholder="blur"
-                    blurDataURL={globalVariables.imagePlaceholder}
-                />
+                <AspectRatio ratio={270 / 180}>
+                    <Image
+                        alt={`Постер к ${anime.name}`}
+                        src={poster}
+                        component={NextImage}
+                        className={classes.poster}
+                        width={270}
+                        height={180}
+                        placeholder="blur"
+                        blurDataURL={globalVariables.imagePlaceholder}
+                    />
+                </AspectRatio>
                 <Group>
                     <Avatar />
                     <Stack>
                         <Title order={3} lineClamp={2}>{anime.name}</Title>
                         <Text lineClamp={1}>
                             {
-                                anime.studios.map((studio) => {
+                                anime.studios.map((studio, index) => {
+                                    const lastElement = anime.studios.length - 1 === index
+
                                     return (
-                                        <span key={studio.id}>{studio.name}</span>
+                                        <span key={studio.id}>{studio.name}{!lastElement && ", "}</span>
                                     )
                                 })
                             }
@@ -38,7 +42,6 @@ export default function AnimeTitleCard({ anime }: { anime: AnimeType }) {
                     </Stack>
                 </Group>
             </Stack>
-
         </div>
     )
 }
