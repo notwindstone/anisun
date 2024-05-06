0e31c0487ffc50e2d3797039588fc991262f9a8b
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-05-06 21:17:56 +0300

    TypeScript checks

--- src/components/HeroContent/CarouselSlides/CarouselSlides.tsx
+++ src/components/HeroContent/CarouselSlides/CarouselSlides.tsx
@@ -1,20 +1,32 @@
 import {Carousel} from "@mantine/carousel";
 import CarouselCard from "@/components/CarouselCard/CarouselCard";
 import {Skeleton} from "@mantine/core";
+import {AnimeType} from "@/types/Shikimori/Responses/Types/AnimeType";
 
-export default function CarouselSlides({ status, carouselSlides, error, data }) {
+export default function CarouselSlides(
+    {
+        status,
+        carouselSlides,
+        error,
+        data
+    } : {
+        status: "success" | "error" | "pending";
+        carouselSlides: unknown[];
+        error: Error | null;
+        data: { animes: AnimeType[] } | undefined;
+    }
+) {
     return carouselSlides.map((_slide, index) => {
             return (
                 <Carousel.Slide key={index}>
                     {
-                        status === 'success'
+                        (status === 'success' && data !== undefined)
                             ? (
                                 <>
                                     <CarouselCard animeTitle={data.animes[index]} />
                                 </>
-                            )
-                            : status === 'error' ? (
-                                <>Error: {error.message}</>
+                            ) : status === 'error' ? (
+                                <>Error: {error?.message}</>
                             ) : (
                                 <Skeleton width={209} height={317} />
                             )
