d762e6c74e297951bcde30c65202127dbe74542b
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-05-08 21:35:29 +0300

    NavBar buttons

--- src/components/SideBar/SideBarButton/SideBarButton.tsx
+++ src/components/SideBar/SideBarButton/SideBarButton.tsx
@@ -1,21 +1,64 @@
 import {Tooltip, UnstyledButton} from "@mantine/core";
 import classes from './SideBarButton.module.css';
-import {IconHome, IconHomeFilled} from "@tabler/icons-react";
+import {
+    IconHome,
+    IconHomeFilled,
+    IconSearch,
+    IconTrendingUp,
+} from "@tabler/icons-react";
+import {useState} from "react";
+
+const navLinks = [
+    {
+        label: 'Главная',
+        icon: <IconHome size={32} stroke={1.5} />,
+        activeIcon: <IconHomeFilled size={32} stroke={3} />,
+    },
+    {
+        label: 'Поиск',
+        icon: <IconSearch size={32} stroke={1.5} />,
+        activeIcon: <IconSearch size={32} stroke={3} />,
+    },
+    {
+        label: 'Популярное',
+        icon: <IconTrendingUp size={32} stroke={1.5} />,
+        activeIcon: <IconTrendingUp size={32} stroke={3} />,
+    }
+]
 
 export default function SideBarButton() {
-    return (
-        <>
+    const [active, setActive] = useState(0)
+    const navButtons = navLinks.map((link, index) => {
+        const isActive = active === index
+
+        return (
             <Tooltip
+                key={link.label}
                 color="gray"
                 withArrow
                 position="right"
-                label="Главная"
+                label={link.label}
                 transitionProps={{ transition: 'fade-right' }}
             >
-                <UnstyledButton className={classes.button}>
-                    <IconHome size={32} stroke={1.5} />
+                <UnstyledButton
+                    className={
+                        `${classes.button} ${isActive && classes.activeButton}`
+                    }
+                    onClick={() => setActive(index)}
+                >
+                    {
+                        isActive
+                            ? link.activeIcon
+                            : link.icon
+                    }
                 </UnstyledButton>
             </Tooltip>
+        )
+    })
+
+    return (
+        <>
+            {navButtons}
         </>
     )
 }
