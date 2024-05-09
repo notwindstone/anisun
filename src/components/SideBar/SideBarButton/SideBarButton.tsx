26c4a68ad367a9fda76a2f8dd5203cfc9bfe80d0
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-05-09 17:40:01 +0300

    Icon chevron animation

--- src/components/SideBar/SideBarButton/SideBarButton.tsx
+++ src/components/SideBar/SideBarButton/SideBarButton.tsx
@@ -1,12 +1,13 @@
 import {Center, Group, Popover, rem, Text, Tooltip, Transition, UnstyledButton} from "@mantine/core";
 import classes from './SideBarButton.module.css';
 import {SideBarLink} from "@/types/SideBarLink";
-import {useContext} from "react";
+import {useContext, useState} from "react";
 import {SideBarLinkContext} from "@/components/SideBar/SideBar";
 import {IconChevronRight} from "@tabler/icons-react";
 
 export default function SideBarButton({ link, order }: { link: SideBarLink, order: number }) {
     const { active, setActive, opened } = useContext(SideBarLinkContext)
+    const [expanded, setExpanded] = useState(false);
     const isActive = active === order
     const isPopover = link.content !== undefined
 
@@ -19,7 +20,10 @@ export default function SideBarButton({ link, order }: { link: SideBarLink, orde
                     ${opened && classes.expandedButton}
                 `
             }
-            onClick={() => setActive(order)}
+            onClick={() => {
+                setExpanded(!expanded)
+                setActive(order)
+            }}
         >
             <Center className={classes.iconWrapper} w={64} h={64}>
                 {
@@ -42,7 +46,14 @@ export default function SideBarButton({ link, order }: { link: SideBarLink, orde
                             </Text>
                             {
                                 isPopover && (
-                                    <IconChevronRight className={classes.chevron} size={24} stroke={1.5} style={styles} />
+                                    <IconChevronRight
+                                        className={
+                                            `${classes.chevron} ${expanded && classes.chevronRotated}`
+                                        }
+                                        size={24}
+                                        stroke={1.5}
+                                        style={styles}
+                                    />
                                 )
                             }
                         </Group>
@@ -53,7 +64,13 @@ export default function SideBarButton({ link, order }: { link: SideBarLink, orde
 
     return isPopover ? (
         <>
-            <Popover width={200} position="right" shadow="md">
+            <Popover
+                opened={expanded}
+                onChange={setExpanded}
+                width={200}
+                position="right"
+                transitionProps={{ transition: 'fade-right', duration: 150 }}
+            >
                 <Popover.Target>
                     {button}
                 </Popover.Target>
