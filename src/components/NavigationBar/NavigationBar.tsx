355e390fe72e0987f809ef1d1f4eec997ee4ca8c
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-05-11 16:12:20 +0300

    NavigationBar sticky position

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -3,30 +3,33 @@
 import {Group, rem, UnstyledButton} from "@mantine/core";
 import {useRouter} from "next/navigation";
 import NProgress from "nprogress";
+import classes from './NavigationBar.module.css';
 
 export default function NavigationBar() {
     const router = useRouter();
 
     return (
-        <Group gap={rem(8)}>
-            <UnstyledButton
-                onClick={() => {
-                    NProgress.start()
-                    router.back()
-                    NProgress.done()
-                }}
-            >
-                Back
-            </UnstyledButton>
-            <UnstyledButton
-                onClick={() => {
-                    NProgress.start()
-                    router.forward()
-                    NProgress.done()
-                }}
-            >
-                Forward
-            </UnstyledButton>
-        </Group>
+        <div className={classes.wrapper}>
+            <Group gap={rem(8)}>
+                <UnstyledButton
+                    onClick={() => {
+                        NProgress.start()
+                        router.back()
+                        NProgress.done()
+                    }}
+                >
+                    Back
+                </UnstyledButton>
+                <UnstyledButton
+                    onClick={() => {
+                        NProgress.start()
+                        router.forward()
+                        NProgress.done()
+                    }}
+                >
+                    Forward
+                </UnstyledButton>
+            </Group>
+        </div>
     )
 }
