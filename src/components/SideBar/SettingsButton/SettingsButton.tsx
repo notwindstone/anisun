2ccd2644a7249b2fa266b78feb2d896dc6f90f0f
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-05-11 21:47:50 +0300

    feat: Settings tabs styling

--- src/components/SideBar/SettingsButton/SettingsButton.tsx
+++ src/components/SideBar/SettingsButton/SettingsButton.tsx
@@ -1,19 +1,36 @@
 import {useState} from "react";
-import {rem, Stack, Tabs, Title} from "@mantine/core";
+import {rem, Stack, Tabs, Text, Title} from "@mantine/core";
+import classes from './SettingsButton.module.css';
 
 export default function SettingsButton() {
     const [activeTab, setActiveTab] = useState<string | null>('general');
 
     return (
         <Stack p={rem(8)} gap={rem(16)}>
-            <Title order={2}>Настройки</Title>
-            <Tabs  variant="pills" radius="sm" value={activeTab} onChange={setActiveTab} orientation="vertical">
+            <Title c="white" order={2}>Настройки</Title>
+            <Tabs
+                classNames={{
+                    root: classes.root,
+                    list: classes.list,
+                    panel: classes.panel,
+                    tab: classes.tab,
+                }}
+                variant="pills"
+                radius="md"
+                value={activeTab}
+                onChange={setActiveTab}
+                orientation="vertical"
+            >
                 <Tabs.List>
-                    <Tabs.Tab value="general">Общие</Tabs.Tab>
-                    <Tabs.Tab value="about">О сайте</Tabs.Tab>
+                    <Tabs.Tab value="general">
+                        <Text c="white">Общие</Text>
+                    </Tabs.Tab>
+                    <Tabs.Tab c="white" value="about">
+                        <Text c="white">О сайте</Text>
+                    </Tabs.Tab>
                 </Tabs.List>
-                <Tabs.Panel value="general">Общие & theme & language</Tabs.Panel>
-                <Tabs.Panel value="about">О сайте & github & license & contact</Tabs.Panel>
+                <Tabs.Panel c="white" value="general">Общие & theme & language</Tabs.Panel>
+                <Tabs.Panel c="white" value="about">О сайте & github & license & contact</Tabs.Panel>
             </Tabs>
         </Stack>
     );
