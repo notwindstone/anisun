b2f6d0d9f5ff9c83a335c2ddb9047866fbd4b567
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-05-12 13:56:26 +0300

    fix: Fix advanced search ignoring checks if first is true

--- src/lib/anilibria/functions/advancedSearch.ts
+++ src/lib/anilibria/functions/advancedSearch.ts
@@ -5,5 +5,5 @@ import {AdvancedSearchInterface} from "@/types/AniLibria/Queries/AdvancedSearchI
 const anilibriaHost = host.api()
 
 export const advancedSearch = async ({ title, year, duration, filter, limit }: AdvancedSearchInterface) => {
-    return (await axios.get(`${anilibriaHost}title/search/advanced?query={names.en} ~= "${title}" or {names.ru} ~= "${title}" and {type.length} == ${duration} and {season.year} == ${year}&filter=${filter}&limit=${limit}`)).data.list[0]
+    return (await axios.get(`${anilibriaHost}title/search/advanced?query=({names.en} ~= "${title}" or {names.ru} ~= "${title}") and {type.length} == ${duration} and {season.year} == ${year}&filter=${filter}&limit=${limit}`)).data.list[0]
 }
