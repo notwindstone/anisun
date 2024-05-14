13e2403476fa453a22df8315bc2638d0c5df082c
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-05-14 13:58:01 +0300

    feat: change database type using .env.local file

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,18 +1,20 @@
-// /*
 import { Pool } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-serverless';
-
-const pool = new Pool({ connectionString: process.env.NEON_DATABASE_URL });
-
-const db = drizzle(pool);
-// */
-
-/*
+import { drizzle as drizzleNeon } from 'drizzle-orm/neon-serverless';
 import postgres from "postgres";
-import {drizzle} from "drizzle-orm/postgres-js";
+import { drizzle as drizzlePostgres } from "drizzle-orm/postgres-js";
 
-const queryClient = postgres(process.env.POSTGRESQL_DATABASE_URL!);
-const db = drizzle(queryClient);
-*/
+const dbType = process.env.DATABASE_TYPE!;
+let db;
 
-export default db
+switch (dbType.toLowerCase()) {
+    case "neon":
+        const pool = new Pool({ connectionString: process.env.NEON_DATABASE_URL });
+        db = drizzleNeon(pool);
+        break;
+    case "postgresql":
+        const queryClient = postgres(process.env.POSTGRESQL_DATABASE_URL!);
+        db = drizzlePostgres(queryClient);
+        break;
+}
+
+export default db;
