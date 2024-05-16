136a6a430abde9947899c1c22decb437db176c18
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-05-16 07:40:38 +0300

    feat: don't show SideBar if user is from mobile

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,14 +1,16 @@
 "use client"
 
 import {CustomThemeContext} from "@/utils/Contexts/Contexts";
-import {useLocalStorage} from "@mantine/hooks";
+import {useLocalStorage, useMediaQuery} from "@mantine/hooks";
 import ThemeSchemeControl from "@/components/ThemeSchemeControl/ThemeSchemeControl";
 import {ThemeType} from "@/types/CustomThemeContext/Theme.type";
 import ColorSchemeControl from "@/components/ColorSchemeControl/ColorSchemeControl";
 import defaultTheme from '@/configs/defaultTheme.json';
 import DecoratedButton from "@/components/DecoratedButton/DecoratedButton";
+import {em} from "@mantine/core";
 
 export default function SideBar() {
+    const isMobile = useMediaQuery(`(max-width: ${em(750)})`);
     const [theme, setTheme] = useLocalStorage<ThemeType>({
         key: 'settings',
         defaultValue: {
@@ -17,7 +19,7 @@ export default function SideBar() {
         },
     })
 
-    return (
+    return isMobile === false && (
         <CustomThemeContext.Provider value={{ theme: theme, setTheme: setTheme }}>
             <DecoratedButton>
                 Кастомная кнопка в сайдбаре (в контексте)
