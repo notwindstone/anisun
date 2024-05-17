45695461aa997ed1fc3ac7e3d5390b56256eec44
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-05-17 17:44:47 +0300

    feat: fetch data based on sortingType

---
+++ src/components/TitleCard/TitleCard.tsx
@@ -0,0 +1,11 @@
+import {Box} from "@mantine/core";
+
+export default function TitleCard({ title }: { title: any }) {
+    return (
+        <>
+            <Box>
+                {title.code}
+            </Box>
+        </>
+    )
+}
--- src/components/TitlesList/TitlesList.tsx
+++ src/components/TitlesList/TitlesList.tsx
@@ -5,17 +5,37 @@ import {useState} from "react";
 import { sorting } from '@/configs/globalVariables.json';
 import TitlesSort from "@/components/TitlesSort/TitlesSort";
 import {SortType} from "@/types/TitlesList/Sort.type";
-import translateAnimeStatus from "@/utils/Translates/translateAnimeStatus";
+import {useQuery} from "@tanstack/react-query";
+import TitleCard from "@/components/TitleCard/TitleCard";
 
 const ALL_TITLES = sorting.all;
 
 export default function TitlesList() {
     const [sortingType, setSortingType] = useState<SortType>(ALL_TITLES.value);
+    const { data, status } = useQuery({
+        queryFn: async () => {
+            return await fetch('https://api.anilibria.tv/v3/title/search?search=' + sortingType + '&filter=id,code')
+                .then(response => response.json())
+        },
+        queryKey: ["titlesList", sortingType]
+    })
+
+    const titles = data?.list.map((title: { id: number }) => {
+        return (
+            <TitleCard key={title.id} title={title} />
+        )
+    })
 
     return (
         <TitlesSortContext.Provider value={{ sortingType: sortingType, setSortingType: setSortingType }}>
             <TitlesSort />
-            {translateAnimeStatus(sortingType)}
+            {
+                status === "pending" ? (
+                    <div>Pending...</div>
+                ) : (
+                    titles
+                )
+            }
         </TitlesSortContext.Provider>
     )
 }
