c5b762c48548abfd312b648cce6ee9bd79e50654
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-05-18 21:02:50 +0300

    feat: show initial animation if section is in viewport

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,8 +3,10 @@
 import {useCallback, useEffect, useState} from "react";
 import {Carousel, Embla} from "@mantine/carousel";
 import {Progress} from "@mantine/core";
+import {useInViewport} from "@mantine/hooks";
 
 export default function Hero() {
+    const { ref, inViewport } = useInViewport();
     const [scrollProgress, setScrollProgress] = useState(0);
     const [embla, setEmbla] = useState<Embla | null>(null);
 
@@ -30,9 +32,22 @@ export default function Hero() {
                 getEmblaApi={setEmbla}
                 initialSlide={2}
             >
-                <Carousel.Slide>1</Carousel.Slide>
-                <Carousel.Slide>2</Carousel.Slide>
-                <Carousel.Slide>3</Carousel.Slide>
+                <Carousel.Slide
+                    style={{
+                        transform: inViewport ? "none" : "translateX(-200px)",
+                        opacity: inViewport ? 1 : 0,
+                        transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
+                    }}
+                    ref={ref}
+                >
+                    134253245234523453245
+                </Carousel.Slide>
+                <Carousel.Slide>
+                    2
+                </Carousel.Slide>
+                <Carousel.Slide>
+                    3
+                </Carousel.Slide>
             </Carousel>
             <Progress
                 value={scrollProgress}
