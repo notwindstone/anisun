f4f008b9c0f5f18bf4258b12c3fdfec644e509c7
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-05-18 23:10:35 +0300

    feat: transition

--- src/components/Hero/HeroCard/HeroCard.tsx
+++ src/components/Hero/HeroCard/HeroCard.tsx
@@ -1,6 +1,6 @@
 import {AnimeType} from "@/types/Shikimori/Responses/Types/Anime.type";
 import {useInViewport} from "@mantine/hooks";
-import {BackgroundImage, Box, Image, Paper, rem, Text, Title, Transition} from "@mantine/core";
+import {Box, Image, Overlay, Paper, rem, Text, Title, Transition} from "@mantine/core";
 import Link from "next/link";
 import classes from './HeroCard.module.css';
 import {variables} from "@/configs/variables";
@@ -14,48 +14,45 @@ export default function HeroCard({ animeTitle }: { animeTitle: AnimeType }) {
     }
 
     return (
-        <div ref={ref}>
-            <Paper
-                ref={ref}
-                component={Link}
-                href={`/titles/${animeTitle?.url?.replace('https://shikimori.one/animes/', '')}`}
-                className={classes.card}
-                radius={0}
+        <Paper
+            ref={ref}
+            component={Link}
+            href={`/titles/${animeTitle?.url?.replace('https://shikimori.one/animes/', '')}`}
+            className={classes.card}
+            radius={0}
+        >
+            <Transition
+                mounted={inViewport}
+                transition="pop"
+                duration={1000}
+                timingFunction="ease"
             >
-                <Image
-                    style={{
-                        opacity: inViewport ? 1 : 0.5,
-                        transition: "1000ms ease",
-                    }}
-                    alt="Anime poster"
-                    src={animeTitle?.poster?.originalUrl}
-                    placeholder="blur"
-                    blurDataURL={variables.imagePlaceholder}
-                    component={NextImage}
-                    className={classes.poster}
-                    radius={0}
-                    fill
-                />
-            </Paper>
-
-                <Transition
-                    mounted={inViewport}
-                    transition="fade-left"
-                    duration={1000}
-                    timingFunction="ease"
-                >
-                    {
-                        (styles) => (
-                            <Box
-                                p={24}
+                {
+                    (styles) => (
+                        <>
+                            <Overlay
+                                className={classes.info}
+                                p={rem(24)}
                                 style={styles}
                             >
                                 <Title>Some title</Title>
                                 <Text>DescriptionDescriptionDescriptionDescription</Text>
-                            </Box>
-                        )
-                    }
-                </Transition>
-        </div>
+                            </Overlay>
+                            <Image
+                                style={styles}
+                                alt="Anime poster"
+                                src={animeTitle?.poster?.originalUrl}
+                                placeholder="blur"
+                                blurDataURL={variables.imagePlaceholder}
+                                component={NextImage}
+                                className={classes.poster}
+                                radius={0}
+                                fill
+                            />
+                        </>
+                    )
+                }
+            </Transition>
+        </Paper>
     )
 }
