aa166e6afb363fb68c215b881deb40d71acc3db1
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-05-19 14:16:38 +0300

    feat!: use server actions to get kodik player so no token will be show on client

---
+++ src/lib/kodik/actions.ts
@@ -0,0 +1,13 @@
+"use server"
+
+import {Client} from "kodikwrapper";
+
+export default async function getKodikPlayer({ shikimoriId }: { shikimoriId: string }) {
+    const kodikClient = new Client({
+        token: process.env.KODIK_TOKEN!,
+    });
+
+    return await kodikClient.search({
+        shikimori_id: parseInt(shikimoriId)
+    }).then((response) => response.results.shift())
+}
