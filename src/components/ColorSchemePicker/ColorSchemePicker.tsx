91fe74c51483bea42fb56db6687f88101a4bcc3a
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-05-21 16:29:16 +0300

    feat: select your own theme colors with ColorInput or ColorPicker

--- src/components/ColorSchemePicker/ColorSchemePicker.tsx
+++ src/components/ColorSchemePicker/ColorSchemePicker.tsx
@@ -1,10 +1,37 @@
-import {ColorPicker} from "@mantine/core";
+"use client";
+
+import {ColorInput, ColorPicker, Stack} from "@mantine/core";
 import {useState} from "react";
+import useCustomTheme from "@/hooks/useCustomTheme";
+import DecoratedButton from "@/components/DecoratedButton/DecoratedButton";
+import classes from './ColorSchemePicker.module.css';
 
 export default function ColorSchemePicker() {
+    const { theme, setTheme } = useCustomTheme();
     const [color, onChange] = useState('#000000');
 
+    function updateColor() {
+        setTheme({ ...theme, color: color })
+    }
+
     return (
-        <ColorPicker format="rgba" value={color} onChange={onChange} />
+        <>
+            <Stack className={classes.stack}>
+                <ColorPicker
+                    value={color}
+                    onChange={onChange}
+                />
+                <ColorInput
+                    value={color}
+                    onChange={onChange}
+                    withPicker={false}
+                    variant="unstyled"
+                    placeholder="Введите цвет"
+                />
+                <DecoratedButton onClick={updateColor}>
+                    Обновить
+                </DecoratedButton>
+            </Stack>
+        </>
     )
 }
