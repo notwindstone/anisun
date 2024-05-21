1455e8542a4c448c8de22a6a5f05f760fe727d68
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-05-21 21:05:25 +0300

    feat: about section in SettingsDropdown

--- src/components/SideBar/SideBarSettings/SideBarSettingsDropdown/SideBarSettingsDropdown.tsx
+++ src/components/SideBar/SideBarSettings/SideBarSettingsDropdown/SideBarSettingsDropdown.tsx
@@ -1,8 +1,9 @@
-import {Popover, rem, SegmentedControl, Stack} from "@mantine/core";
+import {Anchor, Box, Group, Image, Popover, rem, SegmentedControl, Stack, Text, Transition} from "@mantine/core";
 import classes from './SideBarSettingsDropdown.module.css';
 import useCustomTheme from "@/hooks/useCustomTheme";
 import {useState} from "react";
 import {variables} from "@/configs/variables";
+import NextImage from "next/image";
 
 const GENERAL = variables.settings.general
 const ABOUT = variables.settings.about
@@ -16,7 +17,52 @@ export default function SideBarSettingsDropdown() {
     switch (section) {
         case "about":
             content = (
-                <></>
+                <Stack className={classes.stack} align="center">
+                    <Image
+                        alt="Animeth website icon"
+                        src="/favicon.png"
+                        radius="xl"
+                        w={64}
+                        h={64}
+                        component={NextImage}
+                        width={64}
+                        height={64}
+                        placeholder="blur"
+                        blurDataURL={variables.imagePlaceholder}
+                    />
+                    <Text
+                        inline
+                        size={rem(32)}
+                        fw={700}
+                        variant="gradient"
+                        gradient={{ from: 'violet', to: 'indigo', deg: 90 }}
+                    >
+                        ANIMETH
+                    </Text>
+                    <Group>
+                        <Anchor
+                            href="https://github.com/windstone-aristotle-yellow/animeth"
+                            target="_blank"
+                        >
+                            GitHub
+                        </Anchor>
+                        <Anchor
+                            href="https://t.me/windst1"
+                            target="_blank"
+                        >
+                            Telegram
+                        </Anchor>
+                        <Anchor
+                            href="https://github.com/windstone-aristotle-yellow/animeth/blob/main/LICENSE"
+                            target="_blank"
+                        >
+                            License
+                        </Anchor>
+                    </Group>
+                    <Text ta="center">
+                        Сайт для просмотра аниме на основе Next.js и Mantine UI. Более подробная информация находится в репозитории на GitHub.
+                    </Text>
+                </Stack>
             )
             break
         case "general":
@@ -29,11 +75,12 @@ export default function SideBarSettingsDropdown() {
 
     return (
         <Popover.Dropdown>
-            <Stack gap={0}>
+            <Group w={rem(400)} wrap="nowrap" p={rem(8)} align="flex-start" gap={rem(16)}>
                 <SegmentedControl
+                    orientation="vertical"
                     color={theme.color}
                     classNames={{
-                        root: classes.root
+                        root: classes.root,
                     }}
                     withItemsBorders={false}
                     value={section}
@@ -44,7 +91,7 @@ export default function SideBarSettingsDropdown() {
                     ]}
                 />
                 {content}
-            </Stack>
+            </Group>
         </Popover.Dropdown>
     )
 }
