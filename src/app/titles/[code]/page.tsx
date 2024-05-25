dd6b4835cd4136d3fe3b1181202bfa7db9dde85c
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-05-25 14:26:09 +0300

    feat: generate dynamic metadata with shikimori query

--- src/app/titles/[code]/page.tsx
+++ src/app/titles/[code]/page.tsx
@@ -1,6 +1,51 @@
 import {Group, rem, Stack} from "@mantine/core";
+import getShikimoriId from "@/utils/Misc/getShikimoriId";
+import {client} from "@/lib/shikimori/client";
+import {Metadata} from "next";
 
-export default function Page() {
+export async function generateMetadata({ params }: { params: { code: string } }): Promise<Metadata> {
+    const shikimori = client();
+    const shikimoriId = getShikimoriId(params.code);
+
+    const anime = (
+        await shikimori
+            .animes
+            .byId({
+                ids: shikimoriId
+            })
+    ).animes[0];
+
+    const placeholderTitle = 'Просмотр аниме на Animeth';
+    const placeholderDescription = 'На сайте Animeth можно бесплатно и без рекламы смотреть аниме с субтитрами или озвучкой, которая выбирается в плеере';
+
+    if (!anime) {
+        return {
+            title: placeholderTitle,
+            description: placeholderDescription,
+            openGraph: {
+                siteName: 'Animeth',
+                type: "website",
+                title: placeholderTitle,
+                description: placeholderDescription,
+            }
+        };
+    }
+
+    return {
+        title: anime.russian,
+        description: anime.description,
+        openGraph: {
+            siteName: 'Animeth',
+            type: "website",
+            title: anime.russian ?? placeholderTitle,
+            description: anime.description ?? placeholderDescription,
+        }
+    };
+}
+
+export default function Page({ params }: { params: { code: string } }) {
+    const shikimoriId = getShikimoriId(params.code);
+
     return (
         <>
             <Group
--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -1,3 +1,20 @@
-export default function Video() {
+"use client";
 
+import {client} from "@/lib/shikimori/client";
+import {useState} from "react";
+import {useQuery} from "@tanstack/react-query";
+
+export default function Video() {
+    const shikimori = client();
+    const [value, setValue] = useState('Kodik');
+    const { isFetching, data } = useQuery({
+        queryKey: ['anime', id],
+        queryFn: async () => fetchAnime(),
+    });
+
+    return (
+        <>
+
+        </>
+    );
 }
