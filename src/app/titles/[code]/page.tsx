f8db9c16d9104e478f085b23aedd707aad4ea724
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-05-25 14:20:24 +0300

    feat: initial anime title page code

--- src/app/titles/[code]/page.tsx
+++ src/app/titles/[code]/page.tsx
@@ -1,5 +1,20 @@
+import {Group, rem, Stack} from "@mantine/core";
+
 export default function Page() {
     return (
-        <></>
+        <>
+            <Group
+                gap={rem(16)}
+            >
+                <Stack
+                    flex={2}
+                >
+                    <div>1</div>
+                </Stack>
+                <Stack flex={1}>
+                    <div>2</div>
+                </Stack>
+            </Group>
+        </>
     );
 }
