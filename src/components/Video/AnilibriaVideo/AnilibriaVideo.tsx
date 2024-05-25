74b7ee0d0f23244c56330e16d7eadc889ec8434b
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-05-25 18:23:32 +0300

    feat: implement anilibria wrapper lib

--- src/components/Video/AnilibriaVideo/AnilibriaVideo.tsx
+++ src/components/Video/AnilibriaVideo/AnilibriaVideo.tsx
@@ -1,6 +1,7 @@
 import {client} from "@/lib/shikimori/client";
 import {useState} from "react";
 import {useQuery} from "@tanstack/react-query";
+import {anilibria} from "@/lib/anilibria/anilibria";
 
 export default function AnilibriaVideo({ id }: { id: string }) {
     const shikimori = client();
---
+++ src/lib/anilibria/anilibria.ts
@@ -0,0 +1,11 @@
+import {byId} from "@/lib/anilibria/functions/byId";
+import {host} from "@/lib/anilibria/functions/host";
+import {search} from "@/lib/anilibria/functions/search";
+import {advancedSearch} from "@/lib/anilibria/functions/advancedSearch";
+
+export const anilibria = {
+    byId: byId,
+    host: host,
+    search: search,
+    advancedSearch: advancedSearch,
+};
