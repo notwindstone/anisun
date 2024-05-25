b2cd2cfc2766eb2709e906d984cce43981b512eb
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-05-25 21:37:29 +0300

    feat: AnimeInfo styling

--- src/components/AnimeInfo/AnimeInfo.tsx
+++ src/components/AnimeInfo/AnimeInfo.tsx
@@ -3,6 +3,9 @@
 import {client} from "@/lib/shikimori/client";
 import {useQuery} from "@tanstack/react-query";
 import {Group, Skeleton, Stack, Text, Title} from "@mantine/core";
+import classes from './AnimeInfo.module.css';
+import DecoratedButton from "@/components/DecoratedButton/DecoratedButton";
+import {IconDownload, IconShare3} from "@tabler/icons-react";
 
 export default function AnimeInfo({ id }: { id: string }) {
     const shikimori = client();
@@ -17,17 +20,32 @@ export default function AnimeInfo({ id }: { id: string }) {
         })).animes[0];
     }
 
+    if (isPending) {
+        return <>Loading...</>;
+    }
+
     if (error) {
         return <></>;
     }
     console.log(data);
     return (
         <Stack>
-            <Skeleton width="70%" visible={isPending}>
-                <Title lineClamp={2}>{data?.russian} - {data?.name}</Title>
-            </Skeleton>
-            <Group>
-                <Skeleton height={24} visible={isPending}>
+            <Title
+                className={classes.title}
+                order={2}
+                lineClamp={2}
+            >
+                {data?.russian} - {data?.name}
+            </Title>
+            <Group justify="space-between">
+                <Stack gap={0}>
+                    <Text fw={600}>
+                        {
+                            data?.genres.map((genre, index) => {
+                                return `${index ? ', ' : ''}${genre.name}`;
+                            })
+                        }
+                    </Text>
                     <Text lineClamp={1}>
                         {
                             data?.studios.map((studio, index) => {
@@ -35,7 +53,21 @@ export default function AnimeInfo({ id }: { id: string }) {
                             })
                         }
                     </Text>
-                </Skeleton>
+                </Stack>
+                <Group>
+                    <DecoratedButton
+                        leftSection={<IconShare3 />}
+                        onClick={() => {}}
+                    >
+                        Поделиться
+                    </DecoratedButton>
+                    <DecoratedButton
+                        leftSection={<IconDownload />}
+                        onClick={() => {}}
+                    >
+                        Скачать
+                    </DecoratedButton>
+                </Group>
             </Group>
         </Stack>
     );
