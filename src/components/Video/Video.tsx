d4ee2373f67b7c12e78f79fb38efab6da394ad61
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-05-26 11:45:48 +0300

    feat: add notes about player

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -2,13 +2,16 @@
 
 import AnilibriaVideo from "@/components/Video/AnilibriaVideo/AnilibriaVideo";
 import {useState} from "react";
-import {rem, SegmentedControl, Stack} from "@mantine/core";
+import {Alert, rem, SegmentedControl, Stack} from "@mantine/core";
 import useCustomTheme from "@/hooks/useCustomTheme";
 import classes from './Video.module.css';
 import KodikVideo from "@/components/Video/KodikVideo/KodikVideo";
+import {IconInfoCircle} from "@tabler/icons-react";
 
 const ANILIBRIA_PLAYER = "anilibria";
 const KODIK_PLAYER = "kodik";
+const KODIK_DESCRIPTION = 'К сожалению, в плеере Kodik нельзя отключить рекламу. Она встроена в плеер и не зависит от нашего сайта. Зато доступен широкий выбор озвучек!';
+const ANILIBRIA_DESCRIPTION = 'В нашем плеере отсутствует реклама, но озвучка используется только от AniLibria.';
 
 export default function Video({ id }: { id: string }) {
     const { theme } = useCustomTheme();
@@ -23,6 +26,17 @@ export default function Video({ id }: { id: string }) {
                     <AnilibriaVideo id={id} />
                 )
             }
+            <Alert
+                radius="md"
+                variant="light"
+                color="gray"
+                title="Информация о плеере"
+                icon={<IconInfoCircle />}
+            >
+                {
+                    player === KODIK_PLAYER ? KODIK_DESCRIPTION : ANILIBRIA_DESCRIPTION
+                }
+            </Alert>
             <SegmentedControl
                 classNames={{
                     root: classes.root
