7bfa01aa67a1895472535a15dd061856eb8e3d07
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-05-26 11:26:57 +0300

    feat: get anilibria player and torrents data in AnimeInfoDownload component

--- src/components/AnimeInfo/AnimeInfoDownloadVideo/AnimeInfoDownloadVideo.tsx
+++ src/components/AnimeInfo/AnimeInfoDownloadVideo/AnimeInfoDownloadVideo.tsx
@@ -1,16 +1,67 @@
 import {IconDownload} from "@tabler/icons-react";
 import DecoratedButton from "@/components/DecoratedButton/DecoratedButton";
-import {QueryClient} from "@tanstack/react-query";
-
+import {useQuery} from "@tanstack/react-query";
+import {anilibria} from "@/lib/anilibria/anilibria";
+import {client} from "@/lib/shikimori/client";
+import {rem, Skeleton} from "@mantine/core";
 
 export default function AnimeInfoDownloadVideo({ id }: { id: string }) {
-    const queryClient = new QueryClient();
-    const data = queryClient.getQueryData(['anime', 'anilibria', id]);
+    const shikimori = client();
+    const { data, isPending, error } = useQuery({
+        queryKey: ['anime', 'download', id],
+        queryFn: async () => getDownloadLinks(),
+    });
+
+    async function getDownloadLinks() {
+        const shikimoriAnime = (await shikimori.animes.byId({
+            ids: id,
+            filter: [
+                "name",
+                "english",
+                "russian",
+                "airedOn { year month day date }",
+                "duration"
+            ],
+        })).animes[0];
+
+        const shikimoriOriginalName = shikimoriAnime.name;
+        const shikimoriEnglishName = shikimoriAnime.english;
+        const shikimoriRussianName = shikimoriAnime.russian;
+        const shikimoriYear = shikimoriAnime.airedOn?.year;
+        const shikimoriDuration = shikimoriAnime.duration ?? 1;
+        const approximateDuration = `(${shikimoriDuration - 1}, ${shikimoriDuration}, ${shikimoriDuration + 1})`;
+
+        const anilibriaData = await anilibria.advancedSearch(
+            {
+                originalName: shikimoriOriginalName,
+                englishName: shikimoriEnglishName,
+                russianName: shikimoriRussianName,
+                year: shikimoriYear,
+                duration: approximateDuration,
+                filter: 'player.host,player.list[*].hls,torrents',
+                limit: 1,
+            }
+        );
+
+        if (!anilibriaData) {
+            return null;
+        }
+
+        return anilibriaData;
+    }
+
+    if (isPending) {
+        return <Skeleton radius="xl" width={rem(126)} height={rem(36)} />;
+    }
+
+    if (error) {
+        return <>Ошибка: {error.message}</>;
+    }
 
     return (
         <DecoratedButton
             leftSection={<IconDownload />}
-            onClick={() => console.log(data)}
+            onClick={() => console.log(data, isPending, error)}
         >
             Скачать
         </DecoratedButton>
