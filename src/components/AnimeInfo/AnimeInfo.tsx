ad27b97918911adae882c837885783e17cae4e8c
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-05-27 20:48:56 +0300

    feat: use Isomorphic DOMPurify to sanitize descriptionHtml from shikimori data

--- src/components/AnimeInfo/AnimeInfo.tsx
+++ src/components/AnimeInfo/AnimeInfo.tsx
@@ -2,35 +2,26 @@
 
 import {client} from "@/lib/shikimori/client";
 import {useQuery} from "@tanstack/react-query";
-import {Divider, Group, Image, rem, Stack, Text, Title, UnstyledButton} from "@mantine/core";
+import {Divider, Group, Image, rem, Stack, Text, Title} from "@mantine/core";
 import classes from './AnimeInfo.module.css';
 import AnimeInfoDownloadVideo from "@/components/AnimeInfo/AnimeInfoDownloadVideo/AnimeInfoDownloadVideo";
 import AnimeInfoCopyLink from "@/components/AnimeInfo/AnimeInfoCopyLink/AnimeInfoCopyLink";
 import {Suspense} from "react";
 import Link from "next/link";
-import {useDisclosure} from "@mantine/hooks";
 import React from "react";
-import translateAnimeStatus from "@/utils/Translates/translateAnimeStatus";
-import translateAnimeKind from "@/utils/Translates/translateAnimeKind";
+import AnimeInfoDescription from "@/components/AnimeInfo/AnimeInfoDescription/AnimeInfoDescription";
 
 export default function AnimeInfo({ id }: { id: string }) {
-    const [opened, { open, close }] = useDisclosure(false);
     const shikimori = client();
     const { data, isPending, error } = useQuery({
         queryKey: ['anime', 'info', id],
         queryFn: async () => getShikimoriInfo(),
     });
-    console.log(data)
+
     async function getShikimoriInfo() {
         return (await shikimori.animes.byId({
             ids: id,
         })).animes[0];
-    }
-
-    function descriptionOpen() {
-        if (!opened) {
-            open();
-        }
     }
 
     if (isPending) {
@@ -40,15 +31,6 @@ export default function AnimeInfo({ id }: { id: string }) {
     if (error) {
         return <></>;
     }
-
-    const nextEpisodeAt
-        = data?.nextEpisodeAt ? `Следующий эпизод: ${data.nextEpisodeAt} • ` : "";
-    const airedOn
-        = data?.airedOn?.date ? `${data.airedOn.date} • ` : "";
-    const animeStatus
-        = data?.status ? `${translateAnimeStatus({ sortingType: data?.status })} • ` : "";
-    const animeKind
-        = data?.kind ? `${translateAnimeKind(data?.kind)}` : "";
 
     return (
         <Stack gap={rem(8)} className={classes.stack}>
@@ -121,74 +103,9 @@ export default function AnimeInfo({ id }: { id: string }) {
                 </Group>
             </Group>
             <Divider />
-            <Group
-                onClick={descriptionOpen}
-                className={`
-                    ${classes.description} ${opened && classes.expandedDescription}
-                `}
-            >
-                <Stack gap={rem(8)}>
-                    <Text className={classes.statsText}>
-                        {`${nextEpisodeAt}${airedOn}${animeStatus}${animeKind}`}
-                    </Text>
-                    {
-                        data?.description && (
-                            <>
-                                <Title className={classes.heading} pt={rem(8)} order={4}>ОПИСАНИЕ</Title>
-                                <Text>{data.description}</Text>
-                            </>
-                        )
-                    }
-                    <Title className={classes.heading} order={4}>ИНФОРМАЦИЯ</Title>
-                    {
-                        (data?.episodes && data?.episodesAired) && (
-                            <Text>
-                                Эпизоды: {data.episodes} / {data.episodesAired}
-                            </Text>
-                        )
-                    }
-                    {
-                        data?.duration && (
-                            <Text>
-                                Длительность эпизода: {data.duration}
-                            </Text>
-                        )
-                    }
-                    {
-                        data?.rating && (
-                            <Text>
-                                Рейтинг: {data.rating}
-                            </Text>
-                        )
-                    }
-                    {
-                        data?.japanese && (
-                            <Text>
-                                Японское название: {data.japanese}
-                            </Text>
-                        )
-                    }
-                    {
-                        data?.english && (
-                            <Text>
-                                Английское название: {data.english}
-                            </Text>
-                        )
-                    }
-                    {
-                        data?.synonyms?.length > 0 && (
-                            <Text>
-                                Другие названия: {data.synonyms.map((synonym) => (
-                                    <span>{synonym}</span>
-                            ))}
-                            </Text>
-                        )
-                    }
-                    <UnstyledButton className={classes.button} onClick={close}>
-                        Свернуть
-                    </UnstyledButton>
-                </Stack>
-            </Group>
+            <Suspense fallback={<p>Loading...</p>}>
+                <AnimeInfoDescription data={data} />
+            </Suspense>
         </Stack>
     );
 }
---
+++ src/components/AnimeInfo/AnimeInfoDescription/AnimeInfoDescription.tsx
@@ -0,0 +1,106 @@
+import {AnimeType} from "@/types/Shikimori/Responses/Types/Anime.type";
+import classes from "@/components/AnimeInfo/AnimeInfo.module.css";
+import {Group, rem, Stack, Text, Title, UnstyledButton} from "@mantine/core";
+import React from "react";
+import translateAnimeStatus from "@/utils/Translates/translateAnimeStatus";
+import translateAnimeKind from "@/utils/Translates/translateAnimeKind";
+import {useDisclosure} from "@mantine/hooks";
+import DOMPurify from "isomorphic-dompurify";
+
+export default function AnimeInfoDescription({ data }: { data: AnimeType }) {
+    const [opened, { open, close }] = useDisclosure(false);
+
+    let cleanDescription;
+
+    if (data?.descriptionHtml) {
+        cleanDescription = DOMPurify.sanitize(data.descriptionHtml);
+    }
+
+    function descriptionOpen() {
+        if (!opened) {
+            open();
+        }
+    }
+
+    const nextEpisodeAt
+        = data?.nextEpisodeAt ? `Следующий эпизод: ${data.nextEpisodeAt} • ` : "";
+    const airedOn
+        = data?.airedOn?.date ? `${data.airedOn.date} • ` : "";
+    const animeStatus
+        = data?.status ? `${translateAnimeStatus({ sortingType: data?.status })} • ` : "";
+    const animeKind
+        = data?.kind ? `${translateAnimeKind(data?.kind)}` : "";
+
+    return (
+        <Group
+            onClick={descriptionOpen}
+            className={`
+                    ${classes.description} ${opened && classes.expandedDescription}
+                `}
+        >
+            <Stack gap={rem(8)}>
+                <Text className={classes.statsText}>
+                    {`${nextEpisodeAt}${airedOn}${animeStatus}${animeKind}`}
+                </Text>
+                {
+                    cleanDescription && (
+                        <>
+                            <Title className={classes.heading} pt={rem(8)} order={4}>ОПИСАНИЕ</Title>
+                            <Text
+                                dangerouslySetInnerHTML={{ __html: cleanDescription }}
+                            />
+                        </>
+                    )
+                }
+                <Title className={classes.heading} order={4}>ИНФОРМАЦИЯ</Title>
+                {
+                    (data?.episodes && data?.episodesAired) && (
+                        <Text>
+                            Эпизоды: {data.episodes} / {data.episodesAired}
+                        </Text>
+                    )
+                }
+                {
+                    data?.duration && (
+                        <Text>
+                            Длительность эпизода: {data.duration}
+                        </Text>
+                    )
+                }
+                {
+                    data?.rating && (
+                        <Text>
+                            Рейтинг: {data.rating}
+                        </Text>
+                    )
+                }
+                {
+                    data?.japanese && (
+                        <Text>
+                            Японское название: {data.japanese}
+                        </Text>
+                    )
+                }
+                {
+                    data?.english && (
+                        <Text>
+                            Английское название: {data.english}
+                        </Text>
+                    )
+                }
+                {
+                    data?.synonyms?.length > 0 && (
+                        <Text>
+                            Другие названия: {data.synonyms.map((synonym) => (
+                            <span>{synonym}</span>
+                        ))}
+                        </Text>
+                    )
+                }
+                <UnstyledButton className={classes.button} onClick={close}>
+                    Свернуть
+                </UnstyledButton>
+            </Stack>
+        </Group>
+    );
+}
