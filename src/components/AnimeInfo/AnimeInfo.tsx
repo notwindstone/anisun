35ab8976f39c7e00a918f2d0bad825329a69a467
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-05-27 18:42:40 +0300

    feat: show genre and studio names as links

--- src/components/AnimeInfo/AnimeInfo.tsx
+++ src/components/AnimeInfo/AnimeInfo.tsx
@@ -2,11 +2,12 @@
 
 import {client} from "@/lib/shikimori/client";
 import {useQuery} from "@tanstack/react-query";
-import {Group, Stack, Text, Title} from "@mantine/core";
+import {Group, Image, rem, Stack, Text, Title} from "@mantine/core";
 import classes from './AnimeInfo.module.css';
 import AnimeInfoDownloadVideo from "@/components/AnimeInfo/AnimeInfoDownloadVideo/AnimeInfoDownloadVideo";
 import AnimeInfoCopyLink from "@/components/AnimeInfo/AnimeInfoCopyLink/AnimeInfoCopyLink";
 import {Suspense} from "react";
+import Link from "next/link";
 
 export default function AnimeInfo({ id }: { id: string }) {
     const shikimori = client();
@@ -39,22 +40,59 @@ export default function AnimeInfo({ id }: { id: string }) {
                 {data?.russian} - {data?.name}
             </Title>
             <Group justify="space-between">
-                <Stack gap={0}>
-                    <Text fw={600}>
-                        {
-                            data?.genres.map((genre, index) => {
-                                return `${index ? ', ' : ''}${genre.name}`;
-                            })
-                        }
-                    </Text>
-                    <Text lineClamp={1}>
-                        {
-                            data?.studios.map((studio, index) => {
-                                return `${index ? ', ' : ''}${studio.name}`;
-                            })
-                        }
-                    </Text>
-                </Stack>
+                <Group gap={rem(8)}>
+                    <Link
+                        href={`/titles?studio=${data?.studios?.[0]?.name}`}
+                    >
+                        <Image
+                            radius="xs"
+                            className={classes.studioLogo}
+                            w="auto"
+                            h={40}
+                            src={data?.studios?.[0]?.imageUrl}
+                        />
+                    </Link>
+                    <Stack gap={0}>
+                        <Text fw={600}>
+                            {
+                                data?.genres.map((genre, index) => {
+                                    return (
+                                        <>
+                                            <span className={classes.span}>
+                                                {index ? ', ' : ''}
+                                            </span>
+                                            <Link
+                                                className={classes.link}
+                                                href={`/titles?studio=${genre.name}`}
+                                            >
+                                                {genre.name}
+                                            </Link>
+                                        </>
+                                    )
+                                })
+                            }
+                        </Text>
+                        <Text lineClamp={1}>
+                            {
+                                data?.studios.map((studio, index) => {
+                                    return (
+                                        <>
+                                        <span className={classes.span}>
+                                            {index ? ', ' : ''}
+                                        </span>
+                                            <Link
+                                                className={classes.link}
+                                                href={`/titles?studio=${studio.name}`}
+                                            >
+                                                {studio.name}
+                                            </Link>
+                                        </>
+                                    );
+                                })
+                            }
+                        </Text>
+                    </Stack>
+                </Group>
                 <Group>
                     <AnimeInfoCopyLink />
                     <Suspense fallback={<p>Loading button...</p>}>
