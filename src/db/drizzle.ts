6343148886a61b92d194a476bb1aaef3098b051f
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-05-28 12:13:35 +0300

    feat: implement account stats wrapper lib

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,10 +1,10 @@
 import { Pool } from '@neondatabase/serverless';
-import { drizzle as drizzleNeon } from 'drizzle-orm/neon-serverless';
+import {drizzle as drizzleNeon, NeonDatabase} from 'drizzle-orm/neon-serverless';
 import postgres from "postgres";
-import { drizzle as drizzlePostgres } from "drizzle-orm/postgres-js";
+import {drizzle as drizzlePostgres, PostgresJsDatabase} from "drizzle-orm/postgres-js";
 
 const dbType = process.env.DATABASE_TYPE!;
-let db;
+let db: NeonDatabase | PostgresJsDatabase;
 
 switch (dbType.toLowerCase()) {
     case "neon": {
@@ -15,6 +15,11 @@ switch (dbType.toLowerCase()) {
     case "postgresql": {
         const queryClient = postgres(process.env.POSTGRESQL_DATABASE_URL!);
         db = drizzlePostgres(queryClient);
+        break;
+    }
+    default: {
+        const pool = new Pool({connectionString: process.env.NEON_DATABASE_URL});
+        db = drizzleNeon(pool);
         break;
     }
 }
---
+++ src/lib/account/stats/reputation.ts
@@ -0,0 +1,24 @@
+"use server";
+
+import db from "@/db/drizzle";
+import { comments } from "@/db/schema";
+import { eq } from 'drizzle-orm';
+
+export const reputation = async ({ userid }: { userid: string }) => {
+    const commentsData =
+        await db
+            .select()
+            .from(comments)
+            .where(eq(comments.userid, userid));
+
+    let likes = 0;
+    let dislikes = 0;
+
+    for (const comment of commentsData) {
+        likes = likes + (comment.likes?.length ?? 0);
+        dislikes = dislikes + (comment.dislikes?.length ?? 0);
+    }
+
+    const reputation = likes - dislikes;
+    return { data: reputation };
+};
