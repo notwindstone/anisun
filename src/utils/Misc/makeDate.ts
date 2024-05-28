72683836a42a99cdcb7bcfe4e62c303aa5377ffb
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-05-28 12:08:38 +0300

    feat: implement makeDate & makeWordEnding utils

---
+++ src/utils/Misc/makeDate.ts
@@ -0,0 +1,35 @@
+import dayjs from "dayjs";
+import 'dayjs/locale/ru';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+export function makeDate(createdAt: string) {
+    dayjs.locale('ru');
+    dayjs.extend(relativeTime);
+
+    const isPreviousYear = dayjs(createdAt).isBefore(dayjs(), 'year');
+
+    if (isPreviousYear) {
+        return dayjs(createdAt).format('D MMMM YYYY в H:mm');
+    }
+
+    const yesterday = dayjs().subtract(1, 'day');
+    const isYesterday = dayjs(createdAt).isSame(yesterday, 'day');
+
+    if (isYesterday) {
+        return dayjs(createdAt).format('Вчера в H:mm');
+    }
+
+    const isToday = dayjs(createdAt).isSame(dayjs(), 'day');
+    const isSameHour = dayjs(createdAt).isSame(dayjs(), 'hour');
+
+    if (isToday) {
+        if (isSameHour) {
+            return dayjs(createdAt).fromNow();
+        }
+
+        return dayjs(createdAt).format('Сегодня в H:mm');
+    }
+
+    // В любой другой день, если не прошлый год, не вчера и не сегодня
+    return dayjs(createdAt).format('D MMMM в H:mm');
+}
