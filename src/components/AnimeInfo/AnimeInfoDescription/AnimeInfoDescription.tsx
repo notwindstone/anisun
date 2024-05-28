f30468be3b8279f8b6e6fe294390707cf1be773a
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-05-28 09:39:56 +0300

    feat: more info in AnimeInfoDescription component

--- src/components/AnimeInfo/AnimeInfoDescription/AnimeInfoDescription.tsx
+++ src/components/AnimeInfo/AnimeInfoDescription/AnimeInfoDescription.tsx
@@ -1,11 +1,13 @@
 import {AnimeType} from "@/types/Shikimori/Responses/Types/Anime.type";
 import classes from "@/components/AnimeInfo/AnimeInfo.module.css";
-import {Group, rem, Stack, Text, Title, UnstyledButton} from "@mantine/core";
+import {Container, Group, Image, Overlay, rem, Stack, Text, Title, UnstyledButton} from "@mantine/core";
 import React from "react";
 import translateAnimeStatus from "@/utils/Translates/translateAnimeStatus";
 import translateAnimeKind from "@/utils/Translates/translateAnimeKind";
 import {useDisclosure} from "@mantine/hooks";
 import DOMPurify from "isomorphic-dompurify";
+import {variables} from "@/configs/variables";
+import NextImage from "next/image";
 
 export default function AnimeInfoDescription({ data }: { data: AnimeType }) {
     const [opened, { open, close }] = useDisclosure(false);
@@ -40,21 +42,55 @@ export default function AnimeInfoDescription({ data }: { data: AnimeType }) {
                     ${classes.description} ${opened && classes.expandedDescription}
                 `}
         >
-            <Stack gap={rem(8)}>
-                <Text className={classes.statsText}>
-                    {`${nextEpisodeAt}${airedOn}${animeStatus}${animeKind}`}
-                </Text>
-                {
-                    cleanDescription && (
-                        <>
-                            <Title className={classes.heading} pt={rem(8)} order={4}>ОПИСАНИЕ</Title>
-                            <Text
-                                {...opened ? null : { lineClamp: 1 }}
-                                dangerouslySetInnerHTML={{ __html: cleanDescription }}
-                            />
-                        </>
-                    )
-                }
+            <Stack w="100%" gap={rem(8)}>
+                <Group wrap="nowrap" justify="space-between">
+                    <Text lineClamp={1} className={classes.statsText}>
+                        {`${nextEpisodeAt}${airedOn}${animeStatus}${animeKind}`}
+                    </Text>
+                    {
+                        !opened && (
+                            <Text className={classes.expandText}>Нажмите, чтобы раскрыть</Text>
+                        )
+                    }
+                </Group>
+                <Group
+                    w="100%"
+                    justify="space-between"
+                    align="flex-start"
+                    wrap="nowrap"
+                >
+                    {
+                        cleanDescription && data?.description && (
+                            <Stack gap={rem(8)}>
+                                <Title className={classes.heading} pt={rem(8)} order={4}>ОПИСАНИЕ</Title>
+                                <Text
+                                    {...opened ? null : { lineClamp: 1 }}
+                                    dangerouslySetInnerHTML={{ __html: cleanDescription }}
+                                />
+                            </Stack>
+                        )
+                    }
+                    <Container className={classes.imageWrapper}>
+                        <Overlay
+                            gradient={
+                                opened ? "#00000000" : "linear-gradient(to bottom, rgba(0, 0, 0, 0.5) 0%, rgba(0, 0, 0, 1) 30%)"
+                            }
+                            className={classes.imageOverlay}
+                        />
+                        <Image
+                            alt="Anime poster"
+                            src={data?.poster?.originalUrl}
+                            placeholder="blur"
+                            blurDataURL={variables.imagePlaceholder}
+                            width={225}
+                            height={317}
+                            w={225}
+                            h={317}
+                            component={NextImage}
+                            radius="md"
+                        />
+                    </Container>
+                </Group>
                 <Title className={classes.heading} order={4}>ИНФОРМАЦИЯ</Title>
                 {
                     (data?.episodes && data?.episodesAired) && (
@@ -66,7 +102,7 @@ export default function AnimeInfoDescription({ data }: { data: AnimeType }) {
                 {
                     data?.duration && (
                         <Text>
-                            Длительность эпизода: {data.duration}
+                            Длительность эпизода: {data.duration} мин.
                         </Text>
                     )
                 }
@@ -116,6 +152,26 @@ export default function AnimeInfoDescription({ data }: { data: AnimeType }) {
                                 <span key={fansub}>{index ? ', ' : ''}{fansub}</span>
                             ))}
                         </Text>
+                    )
+                }
+                {
+                    data?.related?.length > 0 && (
+                        <></>
+                    )
+                }
+                {
+                    data?.screenshots?.length > 0 && (
+                        <></>
+                    )
+                }
+                {
+                    data?.videos?.length > 0 && (
+                        <></>
+                    )
+                }
+                {
+                    data?.externalLinks?.length > 0 && (
+                        <></>
                     )
                 }
 
