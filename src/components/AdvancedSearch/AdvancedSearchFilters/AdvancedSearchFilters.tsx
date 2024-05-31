d03b09b92e0355c5f0e6fbda1a1a413fa98fc91c
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-05-31 21:37:37 +0300

    feat: add minor changes

--- src/components/AdvancedSearch/AdvancedSearchFilters/AdvancedSearchFilters.tsx
+++ src/components/AdvancedSearch/AdvancedSearchFilters/AdvancedSearchFilters.tsx
@@ -1,7 +1,23 @@
+import {Select} from "@mantine/core";
+import {useQuery} from "@tanstack/react-query";
+
 export default function AdvancedSearchFilters() {
+    const { data, isPending, error } = useQuery({
+        queryKey: ['genres'],
+        queryFn: getGenres,
+    });
+
+    async function getGenres() {
+
+    }
+
     return (
         <>
-
+            <Select
+                radius="md"
+                placeholder="Жанр"
+                data={['React', 'Angular', 'Vue', 'Svelte']}
+            />
         </>
     );
 }
---
+++ src/lib/shikimori/endpoints/genres.ts
@@ -0,0 +1,30 @@
+import {options} from "@/lib/shikimori/options";
+import axios from "axios";
+import {ResponseInterface} from "@/types/Shikimori/Responses/Interfaces/Response.interface";
+import {OptionType} from "@/types/Shikimori/Queries/Option.type";
+
+export const genres = () => {
+    const filters: OptionType[] = [
+        "id",
+        "kind",
+        "name",
+        "russian",
+    ];
+
+    const all = async () => {
+        const params = options({ filter: filters });
+
+        return await axios
+            .request(params)
+            .then((response: ResponseInterface) => response.data.data)
+            .catch(() => {
+                return {
+                    genres: []
+                };
+            });
+    };
+
+    return {
+        all: all,
+    };
+};
