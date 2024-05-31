e2aeea4e3beed5dc561988e7fbd09f45b1b7f9c9
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-05-31 18:54:22 +0300

    feat(styling): change TrendingCard styles

---
+++ src/components/TrendingGrid/TrendingCard/TrendingCard.tsx
@@ -0,0 +1,81 @@
+import {AnimeType} from "@/types/Shikimori/Responses/Types/Anime.type";
+import {AspectRatio, Badge, Box, Flex, Image, Overlay, Title} from "@mantine/core";
+import useCustomTheme from "@/hooks/useCustomTheme";
+import {getScoreBadgeColor} from "@/utils/Misc/getScoreBadgeColor";
+import {variables} from "@/configs/variables";
+import NextImage from "next/image";
+import classes from './TrendingCard.module.css';
+import Link from "next/link";
+import translateAnimeStatus from "@/utils/Translates/translateAnimeStatus";
+
+export default function TrendingCard({ anime }: { anime: AnimeType }) {
+    const { theme } = useCustomTheme();
+    const animeStatus = anime?.status ?? "";
+    const isAnnounced = animeStatus === 'anons';
+    const scoreBadgeColor = getScoreBadgeColor({ score: anime?.score });
+
+    return (
+        <>
+            <AspectRatio className={classes.aspectRatio} ratio={ 3 / 4 }>
+                <Overlay
+                    backgroundOpacity={0.4}
+                    radius="md"
+                    className={classes.overlay}
+                >
+                    <Badge className={classes.status} color="black">
+                        {translateAnimeStatus({ sortingType: animeStatus, singular: true })}
+                    </Badge>
+                    {
+                        !isAnnounced && (
+                            <Badge
+                                className={classes.score}
+                                color={scoreBadgeColor}
+                            >
+                                {anime?.score}
+                            </Badge>
+                        )
+                    }
+                    <Flex
+                        className={classes.info}
+                        direction="column"
+                        justify="flex-end"
+                        gap="0.25rem"
+                    >
+                        {
+                            !isAnnounced && (
+                                <Badge autoContrast className={classes.episodes} color={theme.color}>
+                                    {anime?.episodesAired} / {anime?.episodes}
+                                </Badge>
+                            )
+                        }
+                        <Title
+                            className={classes.title}
+                            order={3}
+                            lineClamp={isAnnounced ? 3 : 2}
+                        >
+                            {anime?.russian}
+                        </Title>
+                    </Flex>
+                </Overlay>
+                <Box
+                    component={Link}
+                    href={`/titles/${anime?.url.replace('https://shikimori.one/animes/', '')}`}
+                    className={classes.imageWrapper}
+                    w="100%"
+                    h="100%"
+                >
+                    <Image
+                        className={classes.image}
+                        radius="md"
+                        alt="Anime poster"
+                        src={anime?.poster?.originalUrl}
+                        placeholder="blur"
+                        blurDataURL={variables.imagePlaceholder}
+                        fill
+                        component={NextImage}
+                    />
+                </Box>
+            </AspectRatio>
+        </>
+    );
+}
--- src/components/TrendingGrid/TrendingGrid.tsx
+++ src/components/TrendingGrid/TrendingGrid.tsx
@@ -1,26 +1,35 @@
 "use client";
 
-import {Grid, Pagination, Title} from "@mantine/core";
+import {AspectRatio, Grid, Pagination, rem, Skeleton, Stack, Text, Title} from "@mantine/core";
 import {useQuery} from "@tanstack/react-query";
 import {client} from "@/lib/shikimori/client";
-import {useRouter, useSearchParams} from "next/navigation";
+import {usePathname, useRouter, useSearchParams} from "next/navigation";
 import NProgress from "nprogress";
+import useCustomTheme from "@/hooks/useCustomTheme";
+import classes from './TrendingGrid.module.css';
+import TrendingCard from "@/components/TrendingGrid/TrendingCard/TrendingCard";
+
+const TOTAL_PAGES = 500;
+const LIMIT = 32;
+const PLACEHOLDER_DATA = Array.from({ length: LIMIT });
 
 export default function TrendingGrid() {
+    const { theme } = useCustomTheme();
+    const pathname = usePathname();
     const searchParams = useSearchParams();
     const router = useRouter();
     const unsafePageParam = parseInt(searchParams.get('page') ?? '1');
     const isSafeNumber = unsafePageParam > 0 && !Number.isNaN(unsafePageParam);
     const page = isSafeNumber ? unsafePageParam : 1;
     const shikimori = client();
-    const { data, status, error } = useQuery({
+    const { data, isPending, error } = useQuery({
         queryFn: async () => {
             return (
                 await shikimori
                     .animes
                     .list({
                         order: "popularity",
-                        limit: 32,
+                        limit: LIMIT,
                         page: page,
                         filter: [
                             "id",
@@ -35,23 +44,75 @@ export default function TrendingGrid() {
                     })
             ).animes;
         },
-        queryKey: ["trending"]
+        queryKey: ["trending", page]
     });
 
     function pushToNextPage(nextPage: number) {
+        const redirectUrl = `/trending?page=${nextPage}`;
+        const pathnameWithSearchParams = `${pathname}?page=${page}`;
+
+        if (pathnameWithSearchParams === redirectUrl) {
+            return;
+        }
+
         NProgress.start();
         router.push(`/trending?page=${nextPage}`);
     }
 
     return (
         <>
-            <Title c="var(--animeth-text-contrast-color)">
-                Популярное
-            </Title>
-            <Pagination value={page} onChange={pushToNextPage} total={10} />
-            <Grid>
-
-            </Grid>
+            <Stack p={rem(8)}>
+                <Title c="var(--animeth-text-contrast-color)">
+                    Популярное
+                </Title>
+                <Pagination
+                    siblings={3}
+                    radius="md"
+                    autoContrast
+                    color={theme.color}
+                    value={page}
+                    onChange={pushToNextPage}
+                    total={TOTAL_PAGES}
+                />
+                {
+                    error ? (
+                        <Text>Ошибка: {error.message}</Text>
+                    ) : (
+                        <Grid gutter={{ base: 5, xs: 'md', md: 'xl', xl: 50 }}>
+                            {
+                                (isPending && !data) ? (
+                                    PLACEHOLDER_DATA.map((_placeholder, index) => {
+                                        return (
+                                            <Grid.Col key={index} span={{ base: 12, xs: 6, md: 3 }}>
+                                                <AspectRatio ratio={ 3 / 4 }>
+                                                    <Skeleton radius="md" w="100%" h="100%" />
+                                                </AspectRatio>
+                                            </Grid.Col>
+                                        );
+                                    })
+                                ) : data && (
+                                    data.map((anime) => {
+                                        return (
+                                            <Grid.Col key={anime.id} span={{ base: 12, xs: 6, md: 3 }}>
+                                                <TrendingCard anime={anime} />
+                                            </Grid.Col>
+                                        );
+                                    })
+                                )
+                            }
+                        </Grid>
+                    )
+                }
+                <Pagination
+                    siblings={3}
+                    radius="md"
+                    autoContrast
+                    color={theme.color}
+                    value={page}
+                    onChange={pushToNextPage}
+                    total={TOTAL_PAGES}
+                />
+            </Stack>
         </>
     );
 }
