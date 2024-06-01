7a0ddc6371ff60c6b79fe240201300d112dcb7f3
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-06-02 01:04:38 +0300

    fix(experimental): disable tanstack query refetch on window focus (it may be the reason why comments are so buggy)

--- src/utils/Providers/TanstackQueryProviders.tsx
+++ src/utils/Providers/TanstackQueryProviders.tsx
@@ -13,6 +13,7 @@ function makeQueryClient() {
                 // With SSR, we usually want to set some default staleTime
                 // above 0 to avoid refetching immediately on the client
                 staleTime: 60 * 1000,
+                refetchOnWindowFocus: false,
             },
         },
     });
