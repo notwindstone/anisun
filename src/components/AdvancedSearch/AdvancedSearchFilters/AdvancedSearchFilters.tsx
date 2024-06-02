8afe044339f8108314b27df8cc910d3f513ce5cd
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-06-02 19:06:19 +0300

    feat: use initial state values from search params

--- src/components/AdvancedSearch/AdvancedSearchFilters/AdvancedSearchFilters.tsx
+++ src/components/AdvancedSearch/AdvancedSearchFilters/AdvancedSearchFilters.tsx
@@ -4,31 +4,58 @@ import {useDisclosure} from "@mantine/hooks";
 import {useState} from "react";
 import AdvancedSearchFiltersChildren
     from "@/components/AdvancedSearch/AdvancedSearchFilters/AdvancedSearchFiltersChildren/AdvancedSearchFiltersChildren";
+import DecoratedButton from "@/components/DecoratedButton/DecoratedButton";
+import getAllSearchParams from "@/utils/Misc/getAllSearchParams";
+import {usePathname, useRouter, useSearchParams} from "next/navigation";
 
 const FIRST_ANIME_AIRED_ON = 1917;
 
 export default function AdvancedSearchFilters() {
-    const [censored, { toggle: toggleCensored }] = useDisclosure(false);
-    const [durations, setDurations] = useState<string[]>([]);
-    const [demographicGenresValue, setDemographicGenresValue] = useState<string[]>([]);
-    const [genreGenresValue, setGenreGenresValue] = useState<string[]>([]);
-    const [themeGenresValue, setThemeGenresValue] = useState<string[]>([]);
-    const [kinds, setKinds] = useState<string[]>([]);
-    const [limit, setLimit] = useState(20);
-    const [order, setOrder] = useState<string | null>('');
-    const [ratings, setRatings] = useState<string[]>();
-    const [score, setScore] = useState(7);
+    const searchParams = useSearchParams();
+    const pathname = usePathname();
+    const router = useRouter();
+    const allSearchParams = getAllSearchParams(searchParams);
 
+    const initialCensored = allSearchParams.censored === "true";
+    const initialDurations = allSearchParams.durations ?? [];
+    const initialDemographicGenresValue = allSearchParams.demographicGenres ?? [];
+    const initialGenreGenres = allSearchParams.genreGenres ?? [];
+    const initialThemeGenres = allSearchParams.themeGenres ?? [];
+    const initialKinds = allSearchParams.kinds ?? [];
+    const initialLimit = allSearchParams.limit;
+    const initialOrder = allSearchParams.order ?? '';
+    const initialRatings = allSearchParams.ratings ?? [];
+    const initialScore = allSearchParams.score;
+    const initialYear = allSearchParams.year;
+    const initialRangedYears = allSearchParams.rangedYears;
+    const initialYearsRanged = allSearchParams.yearsRanged === "true";
+    const initialSeasons = allSearchParams.seasons ?? [];
+    const initialStatuses = allSearchParams.statuses ?? [];
+    const initialStudio = allSearchParams.studio;
+
+    const [censored, { toggle: toggleCensored }] = useDisclosure(initialCensored);
+    const [durations, setDurations] = useState<string[]>(initialDurations);
+    const [demographicGenresValue, setDemographicGenresValue] = useState<string[]>(initialDemographicGenresValue);
+    const [genreGenresValue, setGenreGenresValue] = useState<string[]>(initialGenreGenres);
+    const [themeGenresValue, setThemeGenresValue] = useState<string[]>(initialThemeGenres);
+    const [kinds, setKinds] = useState<string[]>(initialKinds);
+    const [limit, setLimit] = useState(initialLimit);
+    const [order, setOrder] = useState<string | null>(initialOrder);
+    const [ratings, setRatings] = useState<string[] | undefined>(initialRatings);
+    const [score, setScore] = useState(initialScore);
     const [yearStart, setYearStart] = useState(FIRST_ANIME_AIRED_ON);
-    const currentYear = new Date().getFullYear();
+    const [year, setYear] = useState(initialYear);
+    const [rangedYears, setRangedYears] = useState<[number, number]>(initialRangedYears);
+    const [yearsRanged, { toggle: toggleYearsRanged }] = useDisclosure(initialYearsRanged);
+    const [seasons, setSeasons] = useState<string[]>(initialSeasons);
+    const [statuses, setStatuses] = useState<string[]>(initialStatuses);
+    const [studio, setStudio] = useState<string | null>(initialStudio);
 
-    const [year, setYear] = useState(currentYear);
-    const [rangedYears, setRangedYears] = useState<[number, number]>([yearStart, currentYear]);
-    const [yearsRanged, { toggle: toggleYearsRanged }] = useDisclosure(false);
-    const [seasons, setSeasons] = useState<string[]>([]);
+    function setFilters() {
+        const pathnameWithSearchParams = `${pathname}`;
 
-    const [statuses, setStatuses] = useState<string[]>([]);
-    const [studio, setStudio] = useState<string | null>('');
+        return router.push(pathnameWithSearchParams);
+    }
 
     return (
         <AdvancedSearchFiltersContext.Provider
@@ -71,6 +98,9 @@ export default function AdvancedSearchFilters() {
         >
             <Stack flex={1}>
                 <AdvancedSearchFiltersChildren />
+                <DecoratedButton onClick={setFilters}>
+                    Сохранить
+                </DecoratedButton>
             </Stack>
         </AdvancedSearchFiltersContext.Provider>
     );
--- src/utils/Misc/getAllSearchParams.ts
+++ src/utils/Misc/getAllSearchParams.ts
@@ -1,29 +1,45 @@
 import {ReadonlyURLSearchParams} from "next/navigation";
 
 export default function getAllSearchParams(searchParams: ReadonlyURLSearchParams) {
-    const limit = searchParams.get('limit');
-    const order = searchParams.get('order');
-    const kind = searchParams.get('kind');
-    const status = searchParams.get('status');
-    const season = searchParams.get('season');
-    const score = searchParams.get('score');
-    const duration = searchParams.get('duration');
-    const rating = searchParams.get('rating');
-    const genre = searchParams.get('genre');
-    const studio = searchParams.get('studio');
     const censored = searchParams.get('censored');
+    const durations = searchParams.getAll('durations');
+    const demographicGenres = searchParams.getAll('demographicGenres');
+    const genreGenres = searchParams.getAll('genreGenres');
+    const themeGenres = searchParams.getAll('themeGenres');
+    const kinds = searchParams.getAll('kinds');
+    const limit = parseInt(searchParams.get('limit') ?? '20');
+    const order = searchParams.get('order');
+    const ratings = searchParams.getAll('ratings');
+    const score = parseInt(searchParams.get('score') ?? '7');
+
+    const currentYear = new Date().getFullYear();
+    const year = parseInt(searchParams.get('year') ?? currentYear.toString());
+    const rangedYears: [number, number] = [
+        parseInt(searchParams.getAll('rangedYears')[0]),
+        parseInt(searchParams.getAll('rangedYears')[1])
+    ];
+    const yearsRanged = searchParams.get('yearsRanged');
+
+    const seasons = searchParams.getAll('seasons');
+    const statuses = searchParams.getAll('initialStatuses');
+    const studio = searchParams.get('studio');
 
     return {
+        censored,
+        durations,
+        demographicGenres,
+        genreGenres,
+        themeGenres,
+        kinds,
         limit,
         order,
-        kind,
-        status,
-        season,
+        ratings,
         score,
-        duration,
-        rating,
-        genre,
-        studio,
-        censored,
+        year,
+        rangedYears,
+        yearsRanged,
+        seasons,
+        statuses,
+        studio
     };
 }
