e6bd16ef6c9085ddf49239192dc1ba05aad21352
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-06-11 11:18:37 +0300

    feat: change metadata according to next-pwa docs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type {Metadata, Viewport} from "next";
 import { Inter } from "next/font/google";
 import React from "react";
 import {ColorSchemeScript, createTheme, Group, MantineProvider} from "@mantine/core";
@@ -18,26 +18,45 @@ import MobileNavbar from "@/components/MobileNavbar/MobileNavbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const title = 'Animeth';
-const description = "Сайт для онлайн просмотра аниме на основе Next.JS 14, Mantine UI, Tanstack Query и Drizzle ORM. Плеер сделан на AniLibria API и Kodik, а поиск аниме через Shikimori API.";
+const APP_NAME = 'Animeth';
+const APP_DESCRIPTION = "Сайт для онлайн просмотра аниме на основе Next.JS 14, Mantine UI, Tanstack Query и Drizzle ORM. Плеер сделан на AniLibria API и Kodik, а поиск аниме через Shikimori API.";
 
 const theme = createTheme({
     defaultRadius: "xl",
 });
 
 export const metadata: Metadata = {
+    applicationName: APP_NAME,
+    manifest: "/manifest.json",
+    appleWebApp: {
+        capable: true,
+        statusBarStyle: "default",
+        title: APP_NAME,
+    },
+    formatDetection: {
+        telephone: false,
+    },
     icons: {
         icon: "/favicon.png"
     },
-    title: title,
-    description: description,
+    title: APP_NAME,
+    description: APP_DESCRIPTION,
     openGraph: {
         images: "/banner.png",
         siteName: 'Animeth',
         type: "website",
-        title: title,
-        description: description,
-    }
+        title: APP_NAME,
+        description: APP_DESCRIPTION,
+    },
+    twitter: {
+        card: "summary",
+        title: APP_NAME,
+        description: APP_DESCRIPTION,
+    },
+};
+
+export const viewport: Viewport = {
+    themeColor: "#FFFFFF",
 };
 
 export default function RootLayout({
