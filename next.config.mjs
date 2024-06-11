b5914a2ff9ebab7ccc9f43bca23a1782666ac9b7
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-06-11 11:47:28 +0300

    feat: remove next-pwa package

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,3 @@
-import withPWAInit from "@ducanh2912/next-pwa";
-
-const withPWA = withPWAInit({
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  swcMinify: true,
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  fallbacks: {
-    //image: "/static/images/fallback.png",
-    document: "/offline", // if you want to fallback to a custom page rather than /_offline
-    // font: '/static/font/fallback.woff2',
-    // audio: ...,
-    // video: ...,
-  },
-  workboxOptions: {
-    disableDevLogs: true,
-  },
-  // ... other options you like
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -35,4 +12,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withPWA(nextConfig);
+export default nextConfig;
