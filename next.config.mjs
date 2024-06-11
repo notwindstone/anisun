f8a572dbd4eabe7c51141d856561dd402d4c9c73
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-06-11 11:29:19 +0300

    fix: try to fix service workers not starting

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,8 @@ const withPWA = require("@ducanh2912/next-pwa").default({
   reloadOnOnline: true,
   swcMinify: true,
   dest: "public",
+  register: true,
+  skipWaiting: true,
   fallbacks: {
     //image: "/static/images/fallback.png",
     document: "/offline", // if you want to fallback to a custom page rather than /_offline
