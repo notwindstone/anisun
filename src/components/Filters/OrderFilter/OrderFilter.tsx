6943b97541cc65a6971711e7ba463c6b5b78e660
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-06-23 13:57:55 +0300

    feat: translate OrderFilter component's strings

--- src/components/Filters/OrderFilter/OrderFilter.tsx
+++ src/components/Filters/OrderFilter/OrderFilter.tsx
@@ -5,6 +5,7 @@ import classes from '@/components/Filters/FiltersSelect.module.css';
 import calculateColor from "@/utils/Misc/calculateColor";
 import useCustomTheme from "@/hooks/useCustomTheme";
 import {useDisclosure} from "@mantine/hooks";
+import {useTranslations} from "next-intl";
 
 export default memo(function OrderFilter({
     order,
@@ -13,9 +14,16 @@ export default memo(function OrderFilter({
     order: string | null,
     setOrder: Dispatch<SetStateAction<string | null>>
 }) {
+    const translate = useTranslations('Translations');
     const { theme } = useCustomTheme();
     const [focused, { open, close }] = useDisclosure(false);
     const color = calculateColor(theme.color);
+    const orderData = variables.filters.order.map((order) => {
+        return {
+            label: translate(order.label),
+            value: order.value,
+        };
+    });
 
     return (
         <Select
@@ -27,10 +35,10 @@ export default memo(function OrderFilter({
                 }
             }}
             classNames={classes}
-            placeholder="Сортировка"
+            placeholder={translate('component__order-filter__order-label')}
             value={order}
             onChange={setOrder}
-            data={variables.filters.order}
+            data={orderData}
         />
     );
 });
--- src/configs/variables.ts
+++ src/configs/variables.ts
@@ -13,6 +13,17 @@ const TV_SPECIAL = "common__kind-tv-special";
 const PV = "common__kind-pv";
 const CM = "common__kind-cm";
 
+const ID_ASC = "common__order-id-label";
+const ID_DESC = "common__order-id_desc-label";
+const RANKED = "common__order-ranked-label";
+const KIND = "common__order-kind-label";
+const POPULARITY = "common__order-popularity-label";
+const NAME = "common__order-name-label";
+const AIRED_ON = "common__order-aired_on-label";
+const EPISODES = "common__order-episodes-label";
+const STATUS = "common__order-status-label";
+const RANDOM = "common__order-random-label";
+
 export const variables: VariablesType = {
     "imagePlaceholder": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAARCAIAAABmX9r1AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAJkSURBVChTFctJTxNRAADgN29mOl2mi7R0gxYpNgLFBU2KKBAjAS9GjhoTb3rzz3gwXk2IiRduJhKIiWikIkYbYqkESulMd6bttNPZ37yqx+/wEW8PLBWBfHMgdi2kWV4KK5p1KcYsJUnTLJfKItk8Jp6vq4MB8FvdG1TunE6UiOF0ko4NQQiA1Oc73O/UhJeKOPCsrRDtfogmYnzP0MnQhLtvYrYiQaAa8TCj9HkqBTKVr+sV1I8huiCzwzdpUThrGv1xLzor7ztT/l+ZDDm/+nR3L5tvk1sfd0zXiN1SXFA1z3MhVmeKP7BcLdTqkEbts2Ied5UgIO4FKylvPc7Klj386uVrEAhyLdxuCOQM64xTNN3MVx2eK6vP/BOzoumwXRhVxerwECHrWJQsOLe8bPN6jkwoav381juzfBQNe3VHwHfx2jEvf/qWY51u2MHwc3avZQJs4J98kc/vEo0sU9oOudH23ianCK7SIekPs/li3eegluYuhxBfrnC1Hm782fG7kCgiJ0InnEgaOni0dvfhg5Va4WhsPK4JJ/HpW3NL93e+ZIT6OTBlHVLwMHeQmppcvJO2eYNVhfEsvNAQ5MqVelsNj4QTSXe3K1Mz6RBFA0XtxJIxY3QhGInsb7zZfL/B2H1Sp8OQRq2pk7NRt33IQ9gYVUe8pOuaRA0sEhIev88TiFCeUZsTkoura1NX5wuVpo7I0+z3yZEADUxI4UQioapSanoGWRqZvn19gDF3etzv9tqCgExNVXo9UVBkqdVqy7LcaDSIx09WNM1wOuwYWwCAfwEAPBgQ/wkpXTMBYP4CJN1Ov5yNZo0AAAAASUVORK5CYII=",
     "sorting": {
@@ -127,43 +138,43 @@ export const variables: VariablesType = {
     "filters": {
         "order": [
             {
-                "label": "По возрастанию айди",
+                "label": ID_ASC,
                 "value": "id"
             },
             {
-                "label": "По убыванию айди",
+                "label": ID_DESC,
                 "value": "id_desc"
             },
             {
-                "label": "По рейтингу",
+                "label": RANKED,
                 "value": "ranked"
             },
             {
-                "label": "По типу",
+                "label": KIND,
                 "value": "kind"
             },
             {
-                "label": "По популярности",
+                "label": POPULARITY,
                 "value": "popularity"
             },
             {
-                "label": "По алфавиту",
+                "label": NAME,
                 "value": "name"
             },
             {
-                "label": "По дате выхода",
+                "label": AIRED_ON,
                 "value": "aired_on"
             },
             {
-                "label": "По кол-ву эпизодов",
+                "label": EPISODES,
                 "value": "episodes"
             },
             {
-                "label": "По статусу",
+                "label": STATUS,
                 "value": "status"
             },
             {
-                "label": "Случайно",
+                "label": RANDOM,
                 "value": "random"
             },
         ],
