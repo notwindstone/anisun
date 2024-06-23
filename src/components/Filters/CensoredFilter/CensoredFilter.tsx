851ce223a54b8f2ac14d6b447f41459aebb66232
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-06-23 13:40:39 +0300

    feat: translate CensoredFilter component's strings

--- src/components/Filters/CensoredFilter/CensoredFilter.tsx
+++ src/components/Filters/CensoredFilter/CensoredFilter.tsx
@@ -2,6 +2,7 @@ import {Switch} from "@mantine/core";
 import useCustomTheme from "@/hooks/useCustomTheme";
 import {memo} from "react";
 import classes from '@/components/Filters/FiltersSwitch.module.css';
+import {useTranslations} from "next-intl";
 
 export default memo(function CensoredFilter({
     censored,
@@ -11,12 +12,13 @@ export default memo(function CensoredFilter({
     toggleCensored: () => void
 }) {
     const { theme } = useCustomTheme();
+    const translate = useTranslations('Translations');
 
     return (
         <>
             <Switch
                 classNames={classes}
-                label="Включить цензуру"
+                label={translate('component__censored-filter__censure-label')}
                 color={theme.color}
                 checked={censored}
                 onChange={toggleCensored}
