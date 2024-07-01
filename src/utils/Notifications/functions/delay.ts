61a4e5297400e7c6f82ea6a2b0eaf24984f376a8
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-07-01 13:36:43 +0300

    feat: translate notify util messages

--- src/utils/Notifications/functions/delay.ts
+++ src/utils/Notifications/functions/delay.ts
@@ -1,8 +1,22 @@
 import {notifications} from "@mantine/notifications";
 import notificationsData from '../../../configs/notificationsData.json';
 
-export const delay = () => {
+export const delay = (locale: string) => {
     notifications.clean();
 
-    return notifications.show(notificationsData.delay);
+    let message;
+
+    switch (locale) {
+        case "en":
+            message = notificationsData.delay.en;
+            break;
+        case "ru":
+            message = notificationsData.delay.ru;
+            break;
+        default:
+            message = notificationsData.delay.en;
+            break;
+    }
+
+    return notifications.show(message);
 };
--- src/utils/Notifications/functions/incorrectInput.ts
+++ src/utils/Notifications/functions/incorrectInput.ts
@@ -1,8 +1,22 @@
 import {notifications} from "@mantine/notifications";
 import notificationsData from '../../../configs/notificationsData.json';
 
-export const incorrectInput = () => {
+export const incorrectInput = (locale: string) => {
     notifications.clean();
 
-    return notifications.show(notificationsData.incorrectInput);
+    let message;
+
+    switch (locale) {
+        case "en":
+            message = notificationsData.incorrectInput.en;
+            break;
+        case "ru":
+            message = notificationsData.incorrectInput.ru;
+            break;
+        default:
+            message = notificationsData.incorrectInput.en;
+            break;
+    }
+
+    return notifications.show(message);
 };
