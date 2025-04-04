ce9fb0489001f62a7dad4d6c6156a3020e316ff6
notwindstone <notwindstone@gmail.com>
2025-04-04 21:56:44 +0300

    feat: change eslint configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +11,21 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+      "next/core-web-vitals",
+      "next/typescript",
+  ),
+  eslintPluginUnicorn.configs.recommended,
+  {
+    rules: {
+      semi: "error",
+      indent: ["error", 4, { "SwitchCase": 1 }],
+      curly: "error",
+      "object-curly-spacing": ["error", "always"],
+      "comma-dangle": ["error", "always-multiline"],
+      "arrow-parens": ["error", "always"],
+    },
+  }
 ];
 
 export default eslintConfig;
