18b30c5259a309b9e62f9a6911b9692d7aa84452
notwindstone <notwindstone@gmail.com>
2025-04-04 23:45:29 +0300

    feat: add initial backend code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,23 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const ip = new Elysia()
+    .derive(
+        { as: 'global' },
+        ({ server, request }) => ({
+            ip: server?.requestIP(request)
+        })
+    )
+    .get('/ip', ({ ip }) => ip)
+
+const server = new Elysia()
+    .use(ip)
+    .get('/ip', ({ ip }) => ip)
+
+const app = new Elysia().get("/", () => {
+    return {
+        data: "Hello Elysia",
+    }
+}).listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
