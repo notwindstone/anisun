ccbcb1a3f3cdf7d157712ef95f1f98670480db28
notwindstone <notwindstone@gmail.com>
2025-04-04 23:12:55 +0300

    feat: save code

--- src/backend/controllers/message.ts
+++ src/backend/controllers/message.ts
@@ -1,8 +1,16 @@
 import { Elysia, t } from "elysia";
 
 export const messageController = new Elysia({ prefix: '/message' })
-    .get('/', () => 'Hello From Elysia 🦊')
-    .get('/:message', ({ params }) => `Your Message: ${params.message} 🦊`, { params: t.Object({ message: t.String() }) })
+    .get('/', () => {
+        return {
+            message: 'Hello From Elysia 🦊',
+        };
+    })
+    .get('/:message', ({ params }) => {
+        return {
+            message: `Your Message: ${params.message} 🦊`,
+        };
+    }, { params: t.Object({ message: t.String() }) })
     .post('/', ({ body }) => body, {
         body: t.Object({
             name: t.String(),
