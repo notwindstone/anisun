85a1e6ab64ca0cc48a997759001075e1f54bc1ef
notwindstone <notwindstone@gmail.com>
2025-04-13 20:15:06 +0300

    refactor: move globals.css to @/styles (fixes tailwind in production)

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../../styles/globals.css";
 import { ConfigsProvider } from "@/utils/providers/ConfigsProvider";
 import { getCookie } from "@/lib/actions/cookies";
 import TopLoader from "@/components/TopLoader/TopLoader";
