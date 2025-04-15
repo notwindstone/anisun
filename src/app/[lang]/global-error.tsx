ba8629102872eb163a859fff3f364abf24c9aada
notwindstone <notwindstone@gmail.com>
2025-04-15 21:51:08 +0300

    feat: handle global errors with custom page

---
+++ src/app/[lang]/global-error.tsx
@@ -0,0 +1,21 @@
+"use client"; // Error boundaries must be Client Components
+
+export default function GlobalError({
+    error,
+    reset,
+}: {
+    error: Error & { digest?: string }
+    reset: () => void
+}) {
+    console.log(error);
+
+    return (
+        // global-error must include html and body tags
+        <html>
+            <body>
+                <h2>Something went wrong!</h2>
+                <button onClick={() => reset()}>Try again</button>
+            </body>
+        </html>
+    );
+}
