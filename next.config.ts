258bafddeb3386e839375f9f99efef743dc8950d
notwindstone <notwindstone@gmail.com>
2025-05-01 21:53:23 +0300

    fix: fix not working nextConfig

--- next.config.ts
+++ next.config.ts
@@ -2,4 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
+    images: {
+        unoptimized: true,
+    },
 };
+
+export default nextConfig;
