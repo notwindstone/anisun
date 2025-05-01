ddfe0f3da485910485fcb62976c48f7b7699bda8
notwindstone <notwindstone@gmail.com>
2025-05-01 22:26:38 +0300

    perf: use resized image for sidebar logo

--- src/components/Sidebar/SidebarWrapper/SidebarWrapper.tsx
+++ src/components/Sidebar/SidebarWrapper/SidebarWrapper.tsx
@@ -9,7 +9,7 @@ import { PanelLeftClose, PanelLeftOpen } from "lucide-react";
 import { setConfigValuesClient } from "@/utils/configs/setConfigValues";
 import AnimatedGradientText from "@/components/AnimatedGradientText/AnimatedGradientText";
 import { AppName, FaviconBlurredBase64 } from "@/constants/app";
-import favicon from "../../../../public/favicon-original.jpg";
+import favicon from "../../../../public/favicon-x40.jpg";
 import Image from "next/image";
 import Link from "next/link";
 
