66c63934474de2b2570937955201afc16fea3947
notwindstone <notwindstone@gmail.com>
2025-05-02 13:17:49 +0300

    style: fix eslint errors

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 import LocaleSwitcher from "@/components/LocaleSwitcher/LocaleSwitcher";
 import PaletteChanger from "@/components/Theme/PaletteChanger/PaletteChanger";
 import { AccentColors, BaseColors } from "@/constants/tailwind";
-import Button from "@/components/Button/Button";
 import LayoutChanger from "@/components/LayoutChanger/LayoutChanger";
 
 export default async function Home() {
