021ad733a7db273dffb816012e51786d041119c0
notwindstone <notwindstone@gmail.com>
2025-05-03 19:48:10 +0300

    feat: save code

--- src/components/Hero/ClientFetch/ClientFetch.tsx
+++ src/components/Hero/ClientFetch/ClientFetch.tsx
@@ -7,6 +7,11 @@ import { useContext } from "react";
 import { ConfigsContext } from "@/utils/providers/ConfigsProvider";
 import SkeletonCard from "@/components/Hero/SkeletonCard/SkeletonCard";
 
+// I thought about refactoring this function to be
+// reusable, but how do i actually pass async functions here?
+// There might be a way using JSON.stringify on the server
+// And JSON.parse on the client, but to me, it seems cringy
+// TODO: figure it out
 export default function ClientFetch() {
     const { data: { theme, colors: { base } } } = useContext(ConfigsContext);
     const { isPending, error, data } = useQuery({
