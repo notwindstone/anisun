ca688e9fd39b97156d478ae0c7b80146c9d01508
notwindstone <notwindstone@gmail.com>
2025-05-17 20:21:24 +0300

    feat: add episodes list menu to the VidstackPlayer

--- src/components/player/VidstackPlayer/VidstackPlayer.tsx
+++ src/components/player/VidstackPlayer/VidstackPlayer.tsx
@@ -5,6 +5,7 @@ import {
     MediaPlayerInstance,
     MediaProvider,
     useMediaState,
+    Menu,
 } from '@vidstack/react';
 import {
     DefaultAudioLayout,
@@ -17,7 +18,7 @@ import '@vidstack/react/player/styles/default/layouts/video.css';
 import { usePathname, useSearchParams } from "next/navigation";
 import { useRouter } from "nextjs-toploader/app";
 import '@/components/player/VidstackPlayer/VidstackPlayer.global.css';
-import { List } from "lucide-react";
+import { List, SettingsIcon } from "lucide-react";
 
 export default function VidstackPlayer({
     videoSrc,
@@ -39,17 +40,28 @@ export default function VidstackPlayer({
                 title={searchParameters.get("title") ?? undefined}
                 className="!rounded-none"
                 onControlsChange={setControlsShown}
+                viewType="video"
+                aspectRatio={"16 / 9"}
             >
                 <MediaProvider />
                 <DefaultAudioLayout icons={defaultLayoutIcons} />
                 <DefaultVideoLayout
                     icons={defaultLayoutIcons}
                 >
-                    <button
-                        className={`absolute w-9 h-9 rounded-lg flex justify-center items-center z-10 top-1 left-1 bg-transparent transition duration-200 ease-out hover:scale-105 hover:bg-[#fff3] hover:cursor-pointer focus:scale-105 focus:bg-[#fff3] ${controlsShown ? "" : "hidden"}`}
-                    >
-                        <List size={24} />
-                    </button>
+                    <Menu.Root>
+                        <Menu.Button
+                            className={`absolute w-9 h-9 rounded-lg flex justify-center items-center z-10 top-1 left-1 bg-transparent transition duration-200 ease-out hover:scale-105 hover:bg-[#fff3] hover:cursor-pointer focus:scale-105 focus:bg-[#fff3] ${controlsShown ? "" : "hidden"}`}
+                        >
+                            <List size={24} />
+                        </Menu.Button>
+                        <Menu.Items
+                            className="vds-menu-items flex transition max-h-[400px] ml-1 min-w-56 flex-col overflow-y-auto overscroll-y-contain rounded-lg border border-white/10 bg-black/95 p-2.5 font-sans text-[15px] font-medium outline-none"
+                            placement="top"
+                            offset={0}
+                        >
+                            {/* Menu Items + Submenus */}
+                        </Menu.Items>
+                    </Menu.Root>
                 </DefaultVideoLayout>
             </MediaPlayer>
         ),
