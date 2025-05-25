e0d647c2e44343e513538fd76797459f7a79f1eb
notwindstone <notwindstone@gmail.com>
2025-05-25 21:07:20 +0300

    style: add key-spacing rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,26 +7,27 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends(
-      "next/core-web-vitals",
-      "next/typescript",
-  ),
-  eslintPluginUnicorn.configs.recommended,
-  {
-    rules: {
-      semi: "error",
-      indent: ["error", 4, { "SwitchCase": 1 }],
-      curly: "error",
-      "object-curly-spacing": ["error", "always"],
-      "comma-dangle": ["error", "always-multiline"],
-      "arrow-parens": ["error", "always"],
-      "unicorn/filename-case": ["off"],
+    ...compat.extends(
+        "next/core-web-vitals",
+        "next/typescript",
+    ),
+    eslintPluginUnicorn.configs.recommended,
+    {
+        rules: {
+            semi:                    "error",
+            indent:                  ["error", 4, { "SwitchCase": 1 }],
+            curly:                   "error",
+            "object-curly-spacing":  ["error", "always"],
+            "comma-dangle":          ["error", "always-multiline"],
+            "arrow-parens":          ["error", "always"],
+            "unicorn/filename-case": ["off"],
+            "key-spacing":           ["error", { "align": "value" }],
+        },
     },
-  }
 ];
 
 export default eslintConfig;
