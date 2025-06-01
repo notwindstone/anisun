0f21db564cb1070a4286eb034c4bcd9af75002e6
notwindstone <notwindstone@gmail.com>
2025-06-01 17:33:36 +0300

    style: fix eslint error

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { sqliteTable, integer } from "drizzle-orm/sqlite-core"
+import { sqliteTable, integer } from "drizzle-orm/sqlite-core";
 
 export const MALToAnilibriaSchema = sqliteTable('malToAnilibria', {
     idMal:       integer().notNull(),
