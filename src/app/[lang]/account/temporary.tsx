c0e4dfdace7c9acffa7785e592ecbe9652af6579
notwindstone <notwindstone@gmail.com>
2025-06-05 23:39:19 +0300

    feat: save code

--- src/app/[lang]/account/temporary.tsx
+++ src/app/[lang]/account/temporary.tsx
@@ -1,29 +1,37 @@
 "use client";
 
 import Button from "@/components/base/Button/Button";
-import { setCookie } from "cookies-next";
 import { getRelativeDate } from "@/utils/misc/getRelativeDate";
+import { setCookie } from "@/lib/actions/cookies";
+import { ExtensionsCookieKey } from "@/constants/app";
+import { useState } from "react";
 
 export default function Temporary() {
+    const [disabled, setDisabled] = useState(false);
+
     return (
         <Button
-            onClick={() => {
-                setCookie("extensions", JSON.stringify([
-                    {
-                        name: "kodik",
-                        url:  "https://github.com/ame-chan-lol/anisun-kodik-extension/raw/refs/heads/main/dist/bundle.js",
-                    },
-                    {
-                        name: "anilibria",
-                        url:  "https://github.com/ame-chan-lol/anisun-anilibria-extension/raw/refs/heads/main/dist/bundle.js",
-                    },
-                ]), {
-                    expires:  getRelativeDate({ days: 365 }),
-                    path:     "/",
-                    secure:   false,
-                    httpOnly: true,
-                    sameSite: "lax",
+            disabled={disabled}
+            onClick={async () => {
+                setDisabled(true);
+
+                await setCookie({
+                    key:   ExtensionsCookieKey,
+                    value: JSON.stringify([
+                        {
+                            name: "kodik",
+                            url:  "https://github.com/ame-chan-lol/anisun-kodik-extension/raw/refs/heads/main/dist/bundle.js",
+                        },
+                        {
+                            name: "anilibria",
+                            url:  "https://github.com/ame-chan-lol/anisun-anilibria-extension/raw/refs/heads/main/dist/bundle.js",
+                        },
+                    ]),
+                    expiresAt: getRelativeDate({ days: 365 }),
+                    httpOnly:  false,
                 });
+
+                setDisabled(false);
             }}
             label={"add two extensions"}
         >
