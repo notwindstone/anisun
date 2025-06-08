865d85235d823cada7f32a8df3c80e131fd4c765
notwindstone <notwindstone@gmail.com>
2025-06-08 22:21:08 +0300

    feat: disable extensions by default

--- src/utils/configs/getSafeExtensionsValues.ts
+++ src/utils/configs/getSafeExtensionsValues.ts
@@ -32,7 +32,7 @@ export default function getSafeExtensionsValues({
         if (!("isDisabled" in extension)) {
             extensionWithAllProperties = {
                 ...extension,
-                isDisabled: false,
+                isDisabled: true,
             };
         }
 
