6aa15b3052d9d7ca9eaeb438383476f1c90cc411
windstone <notwindstone@gmail.com>
2025-06-09 23:42:34 +0300

    feat: add @tanstack/react-query to shared deps

--- remote-component.config.js
+++ remote-component.config.js
@@ -5,5 +5,6 @@ module.exports = {
     resolve: {
         react:              require("react"),
         "react-dom/client": require("react-dom/client"),
+        "@tanstack/react-query": require("@tanstack/react-query"),
     },
 };
