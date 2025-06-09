5c4025470669651bc4302749c173195561d2cb5d
notwindstone <notwindstone@gmail.com>
2025-06-09 14:53:56 +0300

    feat: remove tanstack query from shared dependencies

--- remote-component.config.js
+++ remote-component.config.js
@@ -3,8 +3,7 @@
  */
 module.exports = {
     resolve: {
-        react:                   require("react"),
-        "react-dom/client":      require("react-dom/client"),
-        "@tanstack/react-query": require("@tanstack/react-query"),
+        react:              require("react"),
+        "react-dom/client": require("react-dom/client"),
     },
 };
