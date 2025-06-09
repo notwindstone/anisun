84f56b353a6033befb515a4d70c84b7ddb28f44f
notwindstone <notwindstone@gmail.com>
2025-06-09 17:12:49 +0300

    feat: add initial code for loading extensions from browser

--- src/components/extensions/ExtensionsBrowser/ExtensionsBrowser.tsx
+++ src/components/extensions/ExtensionsBrowser/ExtensionsBrowser.tsx
@@ -5,8 +5,20 @@ import { ConfigsContext } from "@/utils/providers/ConfigsProvider";
 import { ExtensionsContext } from "@/utils/providers/ExtensionsProvider";
 import { useState } from "react";
 import Button from "@/components/base/Button/Button";
+import { useQuery } from "@tanstack/react-query";
+import { ExtensionsLocalStorageKey } from "@/constants/app";
 
 export default function ExtensionsBrowser() {
+    const { data, isPending, error } = useQuery({
+        queryKey: ["extensions", "browser"],
+        queryFn:  async () => {
+            const response = await fetch("https://raw.githubusercontent.com/notwindstone/anisun-extensions/refs/heads/main/manifests.json");
+            const data = await response.json();
+
+            // todo: make a normal checks
+            return data;
+        },
+    });
     const { base, theme, accent } = useContextSelector(ConfigsContext, (value) => {
         return {
             base:   value.data.colors.base,
@@ -14,9 +26,61 @@ export default function ExtensionsBrowser() {
             accent: value.data.colors.accent,
         };
     });
-    const extensions = useContextSelector(ExtensionsContext, (value) => value.data);
+    const { data: extensions, optimisticallyUpdate: setExtensions } = useContextSelector(ExtensionsContext, (value) => value);
     const [show, setShow] = useState(false);
     console.log(extensions, accent);
+
+    let extensionsNode: React.ReactNode;
+
+    if (isPending) {
+        extensionsNode = (
+            <>loading...</>
+        );
+    }
+
+    if (error) {
+        extensionsNode = (
+            <>error..</>
+        );
+    }
+
+    if (data) {
+        // eslint-disable-next-line
+        extensionsNode = data.map((extensionFromStore: any) => {
+            return (
+                <div key={extensionFromStore.url}>
+                    <div>
+                        {extensionFromStore.name}
+                    </div>
+                    <Button
+                        onClick={() => {
+                            const previousExtensions = extensions ?? [];
+
+                            if (previousExtensions.some((value) => value.url === extensionFromStore.url)) {
+                                return;
+                            }
+
+                            localStorage?.setItem(ExtensionsLocalStorageKey, JSON.stringify([...previousExtensions, extensionFromStore]));
+                            setExtensions?.((state) => {
+                                if (!state) {
+                                    return [extensionFromStore];
+                                }
+
+                                return [
+                                    ...state,
+                                    extensionFromStore,
+                                ];
+                            });
+                        }}
+                        label={`install ${extensionFromStore.name} extension`}
+                    >
+                        install
+                    </Button>
+                </div>
+            );
+        });
+    }
+
     return (
         <div className="flex items-center flex-col gap-2 w-full">
             <div
@@ -64,6 +128,9 @@ export default function ExtensionsBrowser() {
                                     Search
                                 </p>
                             </div>
+                        </div>
+                        <div className="flex gap-2">
+                            {extensionsNode}
                         </div>
                     </div>
                 )
