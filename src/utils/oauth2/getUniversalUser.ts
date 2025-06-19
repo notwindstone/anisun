d340e8d3e116d005f78608f4937f29b8f275ac82
notwindstone <notwindstone@gmail.com>
2025-06-19 19:11:07 +0300

    fix: fix not working nickname getter from anilist api

--- src/utils/oauth2/getUniversalUser.ts
+++ src/utils/oauth2/getUniversalUser.ts
@@ -2,6 +2,20 @@ import { UserType } from "@/types/OAuth2/User.type";
 import { OAuth2ProvidersUserType } from "@/types/OAuth2/OAuth2Providers.type";
 
 const getUsername = (user: OAuth2ProvidersUserType): string => {
+    if (
+        "data" in user && (
+            typeof user.data === "object" &&
+            user.data !== null &&
+            "Viewer" in user.data
+        ) && (
+            typeof user.data.Viewer === "object" &&
+            user.data.Viewer !== null &&
+            "name" in user.data.Viewer
+        )
+    ) {
+        return `${user.data.Viewer.name}`;
+    }
+
     if ("nickname" in user) {
         return user.nickname;
     }
