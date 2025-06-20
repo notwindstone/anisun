ff5aa5a81164a211b5bc66a6e000eb9d835e3e11
notwindstone <notwindstone@gmail.com>
2025-06-20 11:34:58 +0300

    fix: use my nickname as default for anilist library when no data from cookies is provided

--- src/app/[lang]/library/page.tsx
+++ src/app/[lang]/library/page.tsx
@@ -65,7 +65,7 @@ export default async function Page() {
         default: {
             providerLibrary = (
                 <AnilistLibraryWrapper
-                    username={safeAccountValues.username}
+                    username={"windstone"}
                     accessToken={accessToken}
                     tokenProvider={"anilist"}
                 />
