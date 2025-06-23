37713dac00ce56c99495dbd59075f9c1e84dd53c
notwindstone <notwindstone@gmail.com>
2025-06-23 19:10:42 +0300

    fix: fix text selection in mobile navbar button on mobile screens

--- src/components/layout/MobileNavbarButton/MobileNavbarButton.tsx
+++ src/components/layout/MobileNavbarButton/MobileNavbarButton.tsx
@@ -72,7 +72,7 @@ export default function MobileNavbarButton({
             // `null` by default, which means only static routes gonna fully prefetch
             // `true` allows for the full dynamic route prefetch
             prefetch
-            className="mobile-navbar__button shrink-0 flex flex-col gap-2 text-xs xxs:text-sm items-center justify-center w-16 xxs:w-20"
+            className="mobile-navbar__button select-none shrink-0 flex flex-col gap-2 text-xs xxs:text-sm items-center justify-center w-16 xxs:w-20"
             style={{
                 ...(
                     focused === item.href ? {
