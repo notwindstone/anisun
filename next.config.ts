181a0e0e6a7af90e98d8a5d03fbab92288c6b89f
windstone <notwindstone@gmail.com>
2025-06-24 00:08:15 +0300

    feat: react compiler please be a salvation

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
+    experimental: {
+        reactCompiler: true,
+    },
     env: {
         GIT_COMMIT_HASH: process.env.GIT_COMMIT_HASH,
     },
