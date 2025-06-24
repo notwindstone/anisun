f7733dbd473d7aef23be73a92e0971a7380b0589
notwindstone <notwindstone@gmail.com>
2025-06-24 11:30:04 +0300

    perf(maybe): remove shadow from smallcard title

--- src/components/misc/SmallCard/SmallCard.tsx
+++ src/components/misc/SmallCard/SmallCard.tsx
@@ -164,7 +164,7 @@ export default function SmallCard({
                                 </Badge>
                             )
                         }
-                        <p className="text-md text-black dark:text-white text-pretty font-medium drop-shadow-sm line-clamp-3">
+                        <p className="text-md text-black dark:text-white text-pretty font-medium line-clamp-3">
                             {
                                 translate({
                                     text:   name,
