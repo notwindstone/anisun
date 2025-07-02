86bf4a5aba9beda8976d0b4e8dbb84dc81961919
notwindstone <notwindstone@gmail.com>
2025-07-02 12:57:21 +0300

    feat: show favicon based on user preferences

--- src/components/base/Favicon/Favicon.tsx
+++ src/components/base/Favicon/Favicon.tsx
@@ -1,13 +1,21 @@
-import favicon from "@/../public/favicon-x256.webp";
+"use client";
+
+import faviconNonAI from "@/../public/favicon-x256.webp";
+import faviconAI from "@/../public/ai-slop-original.webp";
 import ConfiguredImage from "@/components/base/ConfiguredImage/ConfiguredImage";
+import { useContextSelector } from "use-context-selector";
+import { ConfigsContext } from "@/utils/providers/ConfigsProvider";
 
 export default function Favicon() {
+    const logoType = useContextSelector(ConfigsContext, (value) => value.data.other.logo);
+    const favicon = logoType === "non-ai" ? faviconNonAI : faviconAI;
+
     return (
         <div className="select-none w-10 h-10 rounded-md bg-white ring-2 ring-black dark:ring-white drop-shadow-md overflow-clip relative">
             <ConfiguredImage
                 className="w-10 h-10 transition"
                 src={favicon}
-                alt={"Tenshi Hinanawi, a character from Touhou"}
+                alt={"Arisu Sakayanagi, a character from Classroom of the Elite"}
             />
         </div>
     );
