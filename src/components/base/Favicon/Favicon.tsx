e1fce567d1ce4d67a22d019d25f541afd5826762
notwindstone <notwindstone@gmail.com>
2025-07-04 16:54:01 +0300

    feat: update MAL-to-anilibria database

--- src/components/base/Favicon/Favicon.tsx
+++ src/components/base/Favicon/Favicon.tsx
@@ -1,15 +1,7 @@
-"use client";
-
-import faviconNonAI from "@/../public/favicon-x256.webp";
-import faviconAI from "@/../public/ai-slop-original.webp";
+import favicon from "@/../public/favicon-x256.webp";
 import ConfiguredImage from "@/components/base/ConfiguredImage/ConfiguredImage";
-import { useContextSelector } from "use-context-selector";
-import { ConfigsContext } from "@/utils/providers/ConfigsProvider";
 
 export default function Favicon() {
-    const logoType = useContextSelector(ConfigsContext, (value) => value.data.other.logo);
-    const favicon = logoType === "non-ai" ? faviconNonAI : faviconAI;
-
     return (
         <div className="select-none w-10 h-10 rounded-md bg-white ring-2 ring-black dark:ring-white drop-shadow-md overflow-clip relative">
             <ConfiguredImage
