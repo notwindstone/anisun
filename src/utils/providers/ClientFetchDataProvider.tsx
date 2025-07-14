7fa11f1a5fcfdf78693d78305431ede4a4abe696
notwindstone <notwindstone@gmail.com>
2025-07-14 09:08:27 +0300

    fix: fix not building app

--- src/utils/providers/ClientFetchDataProvider.tsx
+++ src/utils/providers/ClientFetchDataProvider.tsx
@@ -3,7 +3,11 @@
 import { createContext } from "use-context-selector";
 
 export const ClientFetchDataContext = createContext<{
-    data: unknown;
+    // generics won't work with React Contexts.
+    // this is the only solution to actually make this context reusable
+    // I didn't use `any` type in any other place
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    data: any;
 }>({
     data: undefined,
 });
