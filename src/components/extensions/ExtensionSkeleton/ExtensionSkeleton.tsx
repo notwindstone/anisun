78006ab40c2659e8e5b5f52a05217cf3c652dd4f
notwindstone <notwindstone@gmail.com>
2025-07-19 11:00:57 +0300

    feat: use user selected colors for ExtensionSkeleton

--- src/components/extensions/ExtensionSkeleton/ExtensionSkeleton.tsx
+++ src/components/extensions/ExtensionSkeleton/ExtensionSkeleton.tsx
@@ -1,3 +1,8 @@
+import { useContextSelector } from "use-context-selector";
+import { ConfigsContext } from "@/utils/providers/ConfigsProvider";
+import parseTailwindColor from "@/utils/configs/parseTailwindColor";
+import { DarkThemeKey } from "@/constants/configs";
+
 export default function ExtensionSkeleton({
     title,
     description,
@@ -9,9 +14,24 @@ export default function ExtensionSkeleton({
     shouldPulse?: boolean;
     children?: React.ReactNode;
 }) {
+    const { base, theme } = useContextSelector(ConfigsContext, (value) => {
+        return {
+            base:  value.data.colors.base,
+            theme: value.data.theme,
+        };
+    });
+
     return (
         <div
-            className={`flex flex-col gap-4 items-center justify-center h-full w-full bg-neutral-200 dark:bg-neutral-800 ${shouldPulse ? "animate-pulse" : ""}`}
+            className={`flex flex-col gap-4 items-center justify-center h-full w-full ${shouldPulse ? "animate-pulse" : ""}`}
+            style={{
+                backgroundColor: parseTailwindColor({
+                    color: base,
+                    step:  theme === DarkThemeKey
+                        ? 800
+                        : 200,
+                }),
+            }}
         >
             <p className="leading-none text-xl sm:text-4xl font-semibold">
                 {title}
