7fb9a7c5c7af299ab8ad2b77fa57f12f358eaae9
notwindstone <notwindstone@gmail.com>
2025-07-25 21:44:14 +0300

    fix: fix eslint errors

--- src/components/search/AppliedFilters/AppliedFilters.tsx
+++ src/components/search/AppliedFilters/AppliedFilters.tsx
@@ -4,7 +4,7 @@ import { AnyOption } from "@/constants/app";
 import { ConfigsContext } from "@/lib/providers/ConfigsProvider";
 import { DarkThemeKey } from "@/constants/configs";
 import { X } from "lucide-react";
-import {AnilistAllowedFilterKeys, AnilistFilterLabels} from "@/constants/anilist";
+import { AnilistAllowedFilterKeys, AnilistFilterLabels } from "@/constants/anilist";
 import parseTailwindColor from "@/lib/appearance/parseTailwindColor";
 
 export default function AppliedFilters({
